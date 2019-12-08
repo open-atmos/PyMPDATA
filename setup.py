7b062711a67e22e119ec056366e7da68d66955ff
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2019-12-08 19:03:42 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup, find_packages
+from distutils.core import setup
 
 setup(
     name='MPyDATA',
     version='0.0.0',
-    packages=find_packages(exclude=['tests', 'examples', 'tutorials']),
+    packages=['MPyDATA', 'MPyDATA/fields'],
     license='GPL v3',
     long_description=''
 )
