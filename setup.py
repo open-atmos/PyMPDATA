b2e50a28153de808fb27224d234d53e8790c70aa
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2019-12-27 13:53:58 +0100

    fixing setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='MPyDATA',
     version='0.0.0',
-    packages=['MPyDATA', 'MPyDATA/arakawa_c'],
+    packages=['MPyDATA', 'MPyDATA/formulae', 'MPyDATA/arakawa_c', 'MPyDATA/arakawa_c/impl'],
     license='GPL v3',
     long_description=''
 )
