51328546f566d78bb467c4ea80f4a29de0b6cd84
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2020-03-10 23:50:13 +0100

    fixing clock for OSX?

--- MPyDATA/clock.py
+++ MPyDATA/clock.py
@@ -5,15 +5,17 @@ ffi = cffi.FFI()
 ffi.cdef('long clock(void);')
 
 sys = platform.system()
+scale = 1
 
 if sys == 'Windows':
-  libc = ffi.dlopen('msvcrt.dll')
-  scale = 1
-elif sys == 'Linux' or sys == 'Darwin':
-  libc = ffi.dlopen('libc.so.6')
-  scale = 1000
+    libc = ffi.dlopen('msvcrt.dll')
+elif sys == 'Linux':
+    libc = ffi.dlopen('libc.so.6')
+    scale = 1000
+elif sys == 'Darwin':
+    libc = ffi.dlopen('libc.dylib')
 else:
-  raise NotImplementedError()
+    raise NotImplementedError()
 
 clock = libc.clock
 
