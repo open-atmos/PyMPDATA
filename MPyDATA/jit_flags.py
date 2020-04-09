c75a5d446e90cbec0554baee51627733a9fe31c5
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2020-04-09 15:39:13 +0200

    more fixs in Olesik_et_al example; adding warning-ignore logic to jit_flags

--- MPyDATA/jit_flags.py
+++ MPyDATA/jit_flags.py
@@ -5,6 +5,10 @@ Created at 03.2020
 # import os
 # os.environ["NUMBA_DISABLE_JIT"] = "1"
 
+from numba.core.errors import NumbaExperimentalFeatureWarning
+import warnings
+warnings.simplefilter('ignore', category=NumbaExperimentalFeatureWarning)
+
 jit_flags = {
     "fastmath": True,
     "error_model": 'numpy',
