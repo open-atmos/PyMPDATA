f192aa0a5968b4f97ace6a933c71a300a993e3d6
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2020-06-13 22:10:53 +0200

    shifting from CFFI to ctypes in MPI module

--- MPyDATA/mpi.py
+++ MPyDATA/mpi.py
@@ -1,43 +1,60 @@
 import numba
-import cffi
+import ctypes
 import numpy as np
 import platform
 from mpi4py import MPI
 
 
-ffi = cffi.FFI()
-
-if MPI._sizeof(MPI.Comm) == ffi.sizeof('int'):
-    _mpi_comm_t = 'int'
+if MPI._sizeof(MPI.Comm) == ctypes.sizeof(ctypes.c_int):
+    MPI_Comm = ctypes.c_int
 else:
-    _mpi_comm_t = 'void*'
-ffi.cdef(f"typedef {_mpi_comm_t} MPI_Comm;")
-ffi.cdef("int MPI_Initialized(int *flag);")
-ffi.cdef("int MPI_Comm_size(MPI_Comm comm, int *size);")
+    MPI_Comm = ctypes.c_void_p
 
-lib = None  # libmpi loaded by mpi4py
-if platform.system() == 'Windows':
+if platform.system() == 'Linux':
+    lib = 'libmpi.so'
+elif platform.system() == 'Windows'
     lib = 'libmpi.dll'
-libmpi = ffi.dlopen(lib)
+elif platform.system() == 'Darwin'
+    lib = 'libmpi.dylib'
+else:
+    raise NotImplementedError()
+libmpi = ctypes.CDLL(lib)
+
+_MPI_Initialized = libmpi.MPI_Initialized
+_MPI_Initialized.restype = ctypes.c_int
+_MPI_Initialized.argtypes = [ctypes.c_void_p]
 
 _MPI_Comm_size = libmpi.MPI_Comm_size
-_MPI_Initialized = libmpi.MPI_Initialized
+_MPI_Comm_size.restype = ctypes.c_int
+_MPI_Comm_size.argtypes = [MPI_Comm, ctypes.c_void_p]
 
 comm_ptr = MPI._addressof(MPI.COMM_WORLD)
-comm_val = ffi.cast('MPI_Comm*', comm_ptr)[0]
+
+
+# https://stackoverflow.com/questions/61509903/how-to-pass-array-pointer-to-numba-function
+@numba.extending.intrinsic
+def address_as_void_pointer(typingctx, src):
+    """ returns a void pointer from a given memory address """
+    from numba.core import types, cgutils
+    sig = types.voidptr(src)
+
+    def codegen(cgctx, builder, sig, args):
+        return builder.inttoptr(args[0], cgutils.voidptr_t)
+    return sig, codegen
 
 
 @numba.njit()
 def initialized():
     flag = np.empty((1,), dtype=np.int32)
-    status = _MPI_Initialized(ffi.from_buffer(flag))
+    status = _MPI_Initialized(flag.ctypes.data)
     assert status == 0
     return bool(flag[0])
 
 
-# @numba.njit()
+@numba.njit()
 def size():
+    comm = numba.carray(address_as_void_pointer(comm_ptr), (1,), dtype=np.int64)
     value = np.empty(1, dtype=np.int32)
-    status = _MPI_Comm_size(comm_val, ffi.from_buffer(value))
+    status = _MPI_Comm_size(comm[0], value.ctypes.data)
     assert status == 0
     return value[0]
