84d1001dde6eb9f91fdc54343928db3418956c7c
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2020-07-05 18:09:42 +0200

    tiny cleanup in indexers; more logic in test_traversals.py

--- MPyDATA_tests/unit_tests/arakawa_c/test_traversals.py
+++ MPyDATA_tests/unit_tests/arakawa_c/test_traversals.py
@@ -1,6 +1,7 @@
 from MPyDATA.arakawa_c.traversals import Traversals
 from MPyDATA.arakawa_c.meta import meta_halo_valid
 from MPyDATA import Options, ScalarField, VectorField, ConstantBoundaryCondition
+from MPyDATA.arakawa_c.indexers import indexers
 import pytest
 import numba
 import numpy as np
@@ -9,13 +10,32 @@ jit_flags = Options().jit_flags
 
 
 @numba.njit(**jit_flags)
-def _increment_scalar(value, _1, _2, _3, _4):
-    return value + 1
+def cell_id(i, j):
+    if i == -1:
+        return j
+    return 100 * i + j
 
 
 @numba.njit(**jit_flags)
-def _ones_vector(_1, _2, _3):
-    return 1
+def _cell_id_scalar(value, arg_1_vec, arg_2_scl, arg_3_scl, arg_4_scl):
+    focus = arg_1_vec[0]
+    if focus != arg_2_scl[0]:
+        raise Exception()
+    if focus != arg_3_scl[0]:
+        raise Exception()
+    if focus != arg_4_scl[0]:
+        raise Exception()
+    return value + cell_id(*focus)
+
+
+@numba.njit(**jit_flags)
+def _cell_id_vector(arg_1, arg_2, arg_3):
+    focus = arg_1[0]
+    if focus != arg_2[0]:
+        raise Exception()
+    if focus != arg_3[0]:
+        raise Exception()
+    return cell_id(*focus)
 
 
 class TestTraversals:
@@ -25,20 +45,21 @@ class TestTraversals:
     @pytest.mark.parametrize("grid", ((5, 6), (11,)))
     @pytest.mark.parametrize("loop", (True, False))
     def test_apply_scalar(n_threads, halo, grid, loop):
-        if len(grid) == 1 and n_threads > 1:
+        n_dims = len(grid)
+        if n_dims == 1 and n_threads > 1:
             return
 
         # arrange
         traversals = Traversals(grid, halo, jit_flags, n_threads)
         sut = traversals.apply_scalar(loop=loop)
 
-        scl_null_arg_impl = ScalarField.make_null(len(grid)).impl
-        vec_null_arg_impl = VectorField.make_null(len(grid)).impl
+        scl_null_arg_impl = ScalarField.make_null(n_dims).impl
+        vec_null_arg_impl = VectorField.make_null(n_dims).impl
 
-        out = ScalarField(np.zeros(grid), halo, [ConstantBoundaryCondition(np.nan)]*len(grid))
+        out = ScalarField(np.zeros(grid), halo, [ConstantBoundaryCondition(np.nan)]*n_dims)
 
         # act
-        sut(_increment_scalar, _increment_scalar,
+        sut(_cell_id_scalar, _cell_id_scalar,
             *out.impl[0],
             *vec_null_arg_impl[0], *vec_null_arg_impl[1],
             *scl_null_arg_impl[0], *scl_null_arg_impl[1],
@@ -47,7 +68,13 @@ class TestTraversals:
             )
 
         # assert
-        assert (out.get() == (len(grid) if loop else 1)).all()
+        data = out.get()
+        assert data.shape == grid
+        focus = (-halo, -halo)
+        for i in range(halo, halo + grid[0]):
+            for j in (-1,) if n_dims == 1 else range(halo, halo + grid[1]):
+                value = indexers[n_dims].at[0](focus, data, i, j)
+                assert value == (n_dims if loop else 1) * cell_id(i, j)
         assert scl_null_arg_impl[0][0][meta_halo_valid]
         assert vec_null_arg_impl[0][0][meta_halo_valid]
         assert not out.impl[0][0][meta_halo_valid]
@@ -57,17 +84,17 @@ class TestTraversals:
     @pytest.mark.parametrize("halo", (1, 2, 3))
     @pytest.mark.parametrize("grid", ((5, 6), (11,)))
     def test_apply_vector(n_threads, halo, grid):
-        if len(grid) == 1 and n_threads > 1:
+        n_dims = len(grid)
+        if n_dims == 1 and n_threads > 1:
             return
 
         # arrange
         traversals = Traversals(grid, halo, jit_flags, n_threads)
         sut = traversals.apply_vector()
 
-        scl_null_arg_impl = ScalarField.make_null(len(grid)).impl
-        vec_null_arg_impl = VectorField.make_null(len(grid)).impl
+        scl_null_arg_impl = ScalarField.make_null(n_dims).impl
+        vec_null_arg_impl = VectorField.make_null(n_dims).impl
 
-        n_dims = len(grid)
         if n_dims == 1:
             data = (np.zeros(grid[0]+1),)
         elif n_dims == 2:
@@ -78,10 +105,10 @@ class TestTraversals:
         else:
             raise NotImplementedError()
 
-        out = VectorField(data, halo, [ConstantBoundaryCondition(np.nan)]*len(grid))
+        out = VectorField(data, halo, [ConstantBoundaryCondition(np.nan)] * n_dims)
 
         # act
-        sut(_ones_vector, _ones_vector,
+        sut(_cell_id_vector, _cell_id_vector,
             *out.impl[0],
             *scl_null_arg_impl[0], *scl_null_arg_impl[1],
             *vec_null_arg_impl[0], *vec_null_arg_impl[1],
@@ -89,8 +116,18 @@ class TestTraversals:
             )
 
         # assert
-        for i in range(n_dims):
-            assert (out.get_component(i) == 1).all()
+        halos = (
+            (halo-1, halo),
+            (halo, halo-1)
+        )
+        for d in range(n_dims):
+            data = out.get_component(d)
+            focus = [-halos[d][i] for i in range(n_dims)]
+            for i in range(halos[d][0], halos[d][0] + data.shape[0]):
+                for j in (-1,) if n_dims == 1 else range(halos[d][1], halos[d][1] + data.shape[1]):
+                    value = indexers[n_dims].at[0](focus, data, i, j)
+                    assert value == cell_id(i, j)
+
         assert scl_null_arg_impl[0][0][meta_halo_valid]
         assert vec_null_arg_impl[0][0][meta_halo_valid]
         assert not out.impl[0][0][meta_halo_valid]
