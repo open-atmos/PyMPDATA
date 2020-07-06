2325309e5b88a18d1728b53ab321d95d661fcdf9
Michael <olesik.michael@gmail.com>
2020-07-06 14:55:32 +0200

    added wall_times as example

--- MPyDATA_tests/smoke_tests/Olesik_et_al_2020/test_wall_time.py
+++ MPyDATA_tests/smoke_tests/Olesik_et_al_2020/test_wall_time.py
@@ -20,8 +20,8 @@ opt_set = (
 
 
 
-def test_wall_time(n_runs = 2):
-    setup = Setup(nr=default_nr, mixing_ratios_g_kg=np.array([5,]))
+def test_wall_time(n_runs = 2, mrats = [5,], generate=False, print_tab = False):
+    setup = Setup(nr=default_nr, mixing_ratios_g_kg=np.array(mrats))
     table_data = {"opts":[], "values":[]}
     for grid in grid_layout_set:
         norm = [1, ]
@@ -34,14 +34,12 @@ def test_wall_time(n_runs = 2):
                 minimal = np.nanmin(wall_times)
                 minimum_values.append(minimal)
                 i+=1
-            print(minimum_values)
             selected_value = np.min(minimum_values)
-            print(selected_value)
             if opts == {'n_iters': 1}:
                 norm[0] = selected_value
             table_data["opts"].append(str(opts)+ "(" +grid.__class__.__name__+ ")")
             table_data["values"].append(round(selected_value/norm[0],2))
-    make_refdata(table_data, generate=False)
+    make_refdata(data=table_data, generate=generate, print_tab=print_tab)
 
 
 def make_data(setup,grid,opts):
@@ -54,16 +52,18 @@ def make_data(setup,grid,opts):
         wall_time = simulation.step(steps)
         last_step += steps
         result['wall_time'].append(wall_time)
-    print('wt', result['wall_time'])
+    # print('wt', result['wall_time'])
     return result
 
-def make_refdata(data, generate=False):
+def make_refdata(data, generate=False, print_tab=False):
     latex_data = r"\hline" + " Variant  & Elapsed Real Time (wrt upwind) " + r"\\ \hline" + "\n"
     for opt, value in zip(data["opts"], data["values"]):
             latex_data += r"\hline" + f" {opt} & {value} " + r"\\ \hline" + "\n"
     latex_start = r"\begin"+ "\n" +"{table}[]" +"\n" +r"\begin"+ "\n"+ "{tabular}"+ "\n" +"{| l | l |}"+ "\n"
     latex_end = "\end \n {tabular} \n \end \n {table}"
     latex_table = latex_start + latex_data + latex_end
+    if print_tab:
+        print(latex_table)
     with open(pathlib.Path(__file__).parent.joinpath("wall_time_refdata.txt"), "w+" if generate else "r") as f:
         if generate:
             f.write(latex_table)
