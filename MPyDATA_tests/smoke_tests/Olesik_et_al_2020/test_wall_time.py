e89624caf16ae7e39010f078e3ac2dd0c64b0207
Michael <olesik.michael@gmail.com>
2020-07-14 21:40:28 +0200

    changed number of runs so it should finish on travis

--- MPyDATA_tests/smoke_tests/Olesik_et_al_2020/test_wall_time.py
+++ MPyDATA_tests/smoke_tests/Olesik_et_al_2020/test_wall_time.py
@@ -22,7 +22,7 @@ opt_set = (
 
 @pytest.mark.skipif(platform.system() == 'Windows',
                     reason="not enough accuracy on windows code (look MPyDATA/clock.py)")
-def test_wall_time(n_runs=10, mrats=[1.5, ], generate=False, print_tab=False):
+def test_wall_time(n_runs=5, mrats=[1.5, ], generate=False, print_tab=False):
     setup = Setup(nr=default_nr, mixing_ratios_g_kg=np.array(mrats))
     table_data = {"opts": [], "values": []}
     for grid in grid_layout_set:
