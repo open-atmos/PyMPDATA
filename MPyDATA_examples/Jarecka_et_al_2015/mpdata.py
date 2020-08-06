f69fe05ccceb200d9a7e5b312e1c678e22098434
pawel <pawel202@protonmail.com>
2020-08-06 19:47:41 +0200

    correct method names for newer mpdata

--- MPyDATA_examples/Jarecka_et_al_2015/mpdata.py
+++ MPyDATA_examples/Jarecka_et_al_2015/mpdata.py
@@ -8,8 +8,8 @@ from MPyDATA.solver import Solver
 # solver = Solver(stepper=stepper, advectee=advectee, advector=advector)
 
 class mpdata_wrapper:
-    def __init__(self, advector, advectee, shape):
-        self.options = Options(n_iters=2, infinite_gauge=True, flux_corrected_transport=True)
+    def __init__(self, advector, advectee, shape, options):
+        self.options = options
 #         self.stepper = Stepper(options=options, grid=(ny, nx))
         self.stepper = Stepper(options=self.options, grid=shape)
         self.solver = Solver(stepper=self.stepper, advectee=advectee, advector=advector)
@@ -19,20 +19,18 @@ class mpdata_wrapper:
         """
         advector: arr_shape@(ny + 1, nx), arr_shape@(ny, nx + 1)
         """
-        self.solver.curr.get()[:] = advectee
-    #     solver.GC_phys.get_component(0)[:] = advector[0]
-    #     solver.GC_phys.get_component(1)[:] = advector[1]
-        # print('type', type(advector))
-        # print('arr', advector)
+        #curr -> advectee
+        #g_phys -> advector
+        self.solver.advectee.get()[:] = advectee
         if type(advector) is tuple:
             for i, _advector_axis in enumerate(advector):
-                self.solver.GC_phys.get_component(i)[:] = _advector_axis
+                self.solver.advector.get_component(i)[:] = _advector_axis
         else:
-            self.solver.GC_phys.get_component(0)[:] = advector
+            self.solver.advector.get_component(0)[:] = advector
 
 
         self.solver.advance(nt=1) #1 -> flux movement
-        return self.solver.curr.get() 
+        return self.solver.advectee.get() 
         
         
 
