9534a7b2dd18cc647e1ec21523657b638e38c36d
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2020-08-09 01:42:02 +0200

    ... not using find_msvcrt()

--- MPyDATA/clock.py
+++ MPyDATA/clock.py
@@ -1,11 +1,14 @@
 import numba
 import ctypes
 import platform
-from ctypes.util import find_library, find_msvcrt
 
 if platform.system() == 'Windows':
+    from ctypes.util import find_msvcrt
     lib = find_msvcrt()
+    if lib is None:
+        lib = 'msvcrt.dll'
 else:
+    from ctypes.util import find_library
     lib = find_library('c')
 
 clock = ctypes.CDLL(lib).clock
