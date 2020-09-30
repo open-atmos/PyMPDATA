4a57d455fb3f6a145d7937a8cccecaaee8e0bc84
Michael <olesik.michael@gmail.com>
2020-09-30 09:42:21 +0200

    fixed test_dpdc

--- MPyDATA_tests/unit_tests/test_dpdc.py
+++ MPyDATA_tests/unit_tests/test_dpdc.py
@@ -7,10 +7,11 @@ def test_DPDC():
     state = np.array([0, 1, 0])
     C = .5
 
-    mpdata = Factories.constant_1d(state, C, Options(n_iters=2, DPDC=True))
+    mpdata = Factories.constant_1d(state, C, Options(n_iters=2, DPDC=True, flux_corrected_transport=True))
     nt = 1
 
     conserved = np.sum(mpdata.advectee.get())
     mpdata.advance(nt)
+    print(mpdata.advectee.get())
 
     assert np.sum(mpdata.advectee.get()) == conserved
