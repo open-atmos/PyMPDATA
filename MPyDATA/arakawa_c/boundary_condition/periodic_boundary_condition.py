17f0a275e4fc416f46541f2d6d7c6d2170b602ee
Michael <olesik.michael@gmail.com>
2020-10-06 16:54:42 +0200

    caching halo fillers

--- MPyDATA/arakawa_c/boundary_condition/periodic_boundary_condition.py
+++ MPyDATA/arakawa_c/boundary_condition/periodic_boundary_condition.py
@@ -5,17 +5,28 @@ Created at 20.03.2020
 """
 
 import numba
+from functools import lru_cache
+
+
+@lru_cache()
+def _make_scalar(at):
+    @numba.njit()
+    def fill_halos(psi, n, sign):
+        return at(*psi, sign * n, 0)
+    return fill_halos
+
+
+@lru_cache()
+def _make_vector(at):
+    @numba.njit()
+    def fill_halos(psi, n, sign):
+        return at(*psi, sign * n, 0)
+    return fill_halos
 
 
 class PeriodicBoundaryCondition:
     def make_scalar(self, at, _):
-        @numba.njit()
-        def fill_halos(psi, n, sign):
-            return at(*psi, sign * n, 0)
-        return fill_halos
+        return _make_scalar(at)
 
     def make_vector(self, at):
-        @numba.njit()
-        def fill_halos(psi, n, sign):
-            return at(*psi, sign * n, 0)
-        return fill_halos
+        return _make_vector(at)
