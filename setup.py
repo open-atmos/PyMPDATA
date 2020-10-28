aa63aa2058ac7a8d7c2979839bff092e585f6b76
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2020-10-28 23:41:07 +0100

    adding reqs for intel-openmp and icc_rt

--- setup.py
+++ setup.py
@@ -2,9 +2,12 @@ from distutils.core import setup
 from platform import system, architecture
 
 install_requires = []
-if architecture()[0] == '64bit' and system() == 'Windows': 
+if architecture()[0] == '64bit' and system() == 'Windows': # TODO: also available for Linux but hangs GithubActions!
   install_requires += ['tbb>=2020.3.254']
-# TODO: intel-openmp for OSX, icc_rt
+if architecture()[0] == '64bit' and system() == 'Linux': 
+  install_requires += ['icc_rt>=2020.0.133', 'intel-openmp>=2020.0.133']
+if architecture()[0] == '64bit' and system() == 'Darwin': 
+  install_requires += ['intel-openmp>=2019.0']
 
 setup(
     name='PyMPDATA',
