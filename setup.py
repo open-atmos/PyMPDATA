f33d602480dc729601f74eff2491b44b141a1c41
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2020-11-12 11:19:40 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,10 @@ from platform import system, architecture
 install_requires = []
 if architecture()[0] == '64bit' and system() == 'Windows': # TODO: also available for Linux but hangs GithubActions!
   install_requires += ['tbb>=2020.3.254']
+#if architecture()[0] == '64bit' and system() == 'Linux': # TODO: hangs colab!
+#  install_requires += ['icc_rt>=2020.0.133']
 if architecture()[0] == '64bit' and system() == 'Linux': 
-  install_requires += ['icc_rt>=2020.0.133', 'intel-openmp>=2020.0.133']
+  install_requires += ['intel-openmp>=2020.0.133']
 if architecture()[0] == '64bit' and system() == 'Darwin': 
   install_requires += ['intel-openmp>=2019.0']
 
