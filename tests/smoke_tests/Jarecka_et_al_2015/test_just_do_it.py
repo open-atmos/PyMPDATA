3ed053de3930a5391bbf69a289ecdab60d92fa83
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2021-10-11 11:11:08 -0500

    asserts in shallow-water smoke test

--- tests/smoke_tests/Jarecka_et_al_2015/test_just_do_it.py
+++ tests/smoke_tests/Jarecka_et_al_2015/test_just_do_it.py
@@ -1,83 +1,32 @@
 import numpy as np
+import pytest
 from matplotlib import pylab
-from PyMPDATA_examples.Jarecka_et_al_2015 import Settings, Simulation, analytic_equations
+from PyMPDATA_examples.Jarecka_et_al_2015 import Settings, Simulation, formulae, plot_output
+from PyMPDATA_examples.utils.error_norms import L2
 
 
-def test_just_do_it(plot=False):
+@pytest.mark.parametrize("nx", (101, 100))
+@pytest.mark.parametrize("ny", (101, 100))
+def test_just_do_it(nx, ny, plot=False):
     # arrange
     settings = Settings()
+    settings.dx *= settings.nx / nx
+    settings.nx = nx
+    settings.dy *= settings.ny / ny
+    settings.ny = ny
     simulation = Simulation(settings)
     times = (1, 3, 7)
-    lambdas_analytic = analytic_equations.d2_el_lamb_lamb_t_evol(
-        times=[0] + list(times),
-        lamb_x0=settings.lx0,
-        lamb_y0=settings.ly0
-    )[1:]
 
     # act
     output = simulation.run()
 
     # plot
-    cuts = ('x', 'y')
-    fig, axs = pylab.subplots(
-        nrows=len(times),
-        ncols=len(cuts),
-        figsize=(9, 9),
-        constrained_layout=True
-    )
-    momenta = {'x': 'uh', 'y': 'vh'}
-    for i_cut, cut in enumerate(cuts):
-        if cut == 'x':
-            idx = (slice(None, None), slice(settings.nx // 2, settings.nx // 2 + 1))
-            coord = settings.dx * (np.linspace(-settings.nx // 2, settings.nx // 2, settings.nx) + .5)
-            x = coord
-            y = 0
-        else:
-            idx = (slice(settings.ny // 2, settings.ny // 2 + 1), slice(None, None))
-            coord = settings.dy * (np.linspace(-settings.ny // 2, settings.ny // 2, settings.ny) + .5)
-            x = 0
-            y = coord
-        for i_t, t in enumerate(times):
-            h_analytic = Simulation.amplitude(x, y, *lambdas_analytic[i_t, (0, 2)])
-            axs[i_t, i_cut].set_xlim(-8.5, 8.5)
-            axs[i_t, i_cut].set_xticks((-5, 0, 5))
-            axs[i_t, i_cut].plot(coord, output[0]['h'][idx].squeeze(), color='black', label='h @ t=0')
-            axs[i_t, i_cut].plot(coord, output[t]['h'][idx].squeeze(), color='red', label='MPDATA: h')
-            axs[i_t, i_cut].plot(coord, h_analytic,
-                                 color='blue', linestyle=':', label='analytic: h')
-            twin = axs[i_t, i_cut].twinx()
-            q = output[t][momenta[cut]][idx].squeeze()
-            h = output[t]['h'][idx].squeeze()
-            twin.plot(
-                coord,
-                np.where(h > settings.eps, np.divide(q, h, where=h > settings.eps), 0),
-                color='red',
-                linestyle='--',
-                label='MPDATA: q'
-            )
-            twin.plot(
-                coord,
-                np.where(h_analytic > settings.eps,
-                         coord / lambdas_analytic[i_t, 0 + 2 * i_cut] * lambdas_analytic[i_t, 1 + 2 * i_cut],
-                         0),
-                linestyle='-.',
-                label='analytic: q',
-                color='blue'
-            )
-            twin.set_ylabel('u' if cut == 'x' else 'v')
-            twin.set_ylim(-1.2, 1.2)
-            twin.set_yticks((-1, 0, 1))
-            axs[i_t, i_cut].set_xlabel(cut)
-            axs[i_t, i_cut].set_ylim(-.6, .6)
-            axs[i_t, i_cut].set_yticks((-.5, 0, .5))
-            axs[i_t, i_cut].set_ylabel('h')
-            axs[i_t, i_cut].set_title(f"t={t}", y=1.0, pad=-14, x=0.075)
-    legend_props = {'frameon': False, 'fontsize': 'small'}
-    axs[i_t, i_cut].legend(loc='lower right', **legend_props)
-    twin.legend(loc='lower center', **legend_props)
+    plot_data = plot_output(times, output, settings, return_data=True)
     if plot:
         pylab.show()
 
     # assert
-    # TODO #273 + make the plotting code common with the example
+    for key, item in plot_data.items():
+        assert 2**L2(item['h_numeric'], item['h_analytic'], nt=settings.nt) < 5e-3
+        assert 2**L2(item['q_h_numeric'], item['q_h_analytic'], nt=settings.nt) < 5e-2
 
