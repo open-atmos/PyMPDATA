c416522ed740f10c39280b62c8a720a05e6b219b
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2021-12-14 09:35:24 -0600

    fixing docstring refs

--- PyMPDATA/__init__.py
+++ PyMPDATA/__init__.py
@@ -3,7 +3,8 @@ Numba-accelerated Pythonic implementation of Multidimensional Positive Definite
 Advection Transport Algorithm (MPDATA) with examples in Python, Julia and Matlab
 
 PyMPDATA uses staggered grid with the following node placement for
-`PyMPDATA.ScalarField` and `PyMPDATA.VectorField` elements:
+`PyMPDATA.scalar_field.ScalarField` and
+`PyMPDATA.vector_field.VectorField` elements:
 ![](https://github.com/atmos-cloud-sim-uj/PyMPDATA/releases/download/tip/readme_grid.png)
 """
 # pylint: disable=invalid-name
