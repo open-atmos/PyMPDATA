782a9e7512083ef4eebbcca53b612de7038d5ff0
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2022-01-15 12:30:15 -0600

    try with numpy 1.21

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     setup_requires=['setuptools_scm'],
     install_requires=[
         'numba' + ('==0.55.0' if 'CI' in os.environ else ''),
-        'numpy' + ('==1.22.1' if 'CI' in os.environ else ''),
+        'numpy' + ('==1.21' if 'CI' in os.environ else ''),
         'pystrict'
     ],
     author='https://github.com/atmos-cloud-sim-uj/PyMPDATA/graphs/contributors',
