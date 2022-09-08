e5a3d6897a69dea26f8f408a85fa8f5f1494f6a8
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2022-09-08 09:47:13 +0200

    more linter issues addressed

--- setup.py
+++ setup.py
@@ -1,5 +1,3 @@
 from setuptools import setup
 
-setup(
-    use_scm_version={"local_scheme": lambda _: "", "version_scheme": "post-release"}
-)
+setup(use_scm_version={"local_scheme": lambda _: "", "version_scheme": "post-release"})
