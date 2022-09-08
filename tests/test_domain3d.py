b33ec50fdf733bdc7179577e8c7597fcde6ff015
Oleksii Bulenok <oleksii.bulenok@swmansion.com>
2022-09-08 19:01:10 +0200

    Run pre-commit

--- tests/test_domain3d.py
+++ tests/test_domain3d.py
@@ -1,11 +1,11 @@
+import numpy as np
 from PySDM import Builder
 from PySDM.backends import CPU
 from PySDM.impl.mesh import Mesh
-import numpy as np
 from PySDM.initialisation.sampling import spatial_sampling
 
 
-class Env():
+class Env:
     def __init__(self, grid, size):
         self.mesh = Mesh(grid=grid, size=size)
 
@@ -16,8 +16,8 @@ class Env():
         positions = spatial_discretisation.sample(self.mesh.grid, n_sd)
 
         attributes = {
-            'n': np.ones(n_sd),
-            'volume': np.full(n_sd, np.nan),
+            "n": np.ones(n_sd),
+            "volume": np.full(n_sd, np.nan),
         }
         (
             attributes["cell id"],
@@ -32,7 +32,9 @@ def test_domain3d(n_sd=44, grid=(5, 6, 7), size=(50, 60, 70)):
     builder = Builder(n_sd=n_sd, backend=CPU())
     env = Env(grid, size)
     builder.set_environment(env)
-    attributes = env.init_attributes(n_sd, spatial_discretisation=spatial_sampling.Pseudorandom())
+    attributes = env.init_attributes(
+        n_sd, spatial_discretisation=spatial_sampling.Pseudorandom()
+    )
 
     # act
     particulator = builder.build(attributes=attributes, products=())
