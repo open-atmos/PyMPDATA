piotrbartman/PyMDATA

handle changing order of traversing

tests/unit_tests/test_boundary_condition_extrapolated_1d.py
@@ -15,12 +15,16 @@ class TestBoundaryConditionExtrapolated:
         "data",
         (np.array([1, 2, 3, 4], dtype=float), np.array([1, 2, 3, 4], dtype=complex)),
     )
-    def test_1d_scalar(data, n_threads=1, halo=1):
+    def test_1d_scalar(data, n_threads_, halo=1, left_first=True):
         # arrange
         boundary_conditions = (Extrapolated(),)
         field = ScalarField(data, halo, boundary_conditions)
         traversals = Traversals(
-            grid=field.grid, halo=halo, jit_flags=JIT_FLAGS, n_threads=n_threads
+            grid=field.grid,
+            halo=halo,
+            jit_flags=JIT_FLAGS,
+            n_threads=n_threads_,
+            left_first=left_first,
         )
         field.assemble(traversals)
         meta_and_data, fill_halos = field.impl
@@ -36,12 +40,16 @@ class TestBoundaryConditionExtrapolated:
 
     @staticmethod
     @pytest.mark.parametrize("data", (np.array([1, 2, 3, 4], dtype=float),))
-    def test_1d_vector(data, n_threads=1, halo=2):
+    def test_1d_vector(data, n_threads_, halo=2, left_first=True):
         # arrange
         boundary_condition = (Extrapolated(),)
         field = VectorField((data,), halo, boundary_condition)
         traversals = Traversals(
-            grid=field.grid, halo=halo, jit_flags=JIT_FLAGS, n_threads=n_threads
+            grid=field.grid,
+            halo=halo,
+            jit_flags=JIT_FLAGS,
+            n_threads=n_threads_,
+            left_first=left_first,
         )
         field.assemble(traversals)
         meta_and_data, fill_halos = field.impl

tests/unit_tests/test_boundary_condition_periodic_nd.py
@@ -50,8 +50,14 @@ JIT_FLAGS = Options().jit_flags
 
 @lru_cache()
 # pylint: disable-next=redefined-outer-name
-def make_traversals(grid, halo, n_threads):
-    return Traversals(grid=grid, halo=halo, jit_flags=JIT_FLAGS, n_threads=n_threads)
+def make_traversals(grid, halo, n_threads, left_first):
+    return Traversals(
+        grid=grid,
+        halo=halo,
+        jit_flags=JIT_FLAGS,
+        n_threads=n_threads,
+        left_first=left_first,
+    )
 
 
 class TestPeriodicBoundaryCondition:
@@ -67,26 +73,28 @@ class TestPeriodicBoundaryCondition:
     @pytest.mark.parametrize("halo", (1, 2, 3))
     @pytest.mark.parametrize("side", (LEFT, RIGHT))
     @pytest.mark.parametrize("dim", DIMENSIONS)
-    # pylint: disable-next=redefined-outer-name
-    def test_scalar(data, halo, side, n_threads, dim):
+    # pylint: disable-next=redefined-outer-name,too-many-arguments
+    def test_scalar(data, halo, side, n_threads_, dim, left_first=True):
         n_dims = len(data.shape)
         if n_dims == 1 and dim != INNER:
             return
         if n_dims == 2 and dim == MID3D:
             return
-        if n_dims == 1 and n_threads > 1:
+        if n_dims == 1 and n_threads_ > 1:
             return
 
         # arrange
         field = ScalarField(data, halo, tuple(Periodic() for _ in range(n_dims)))
-        traversals = make_traversals(grid=field.grid, halo=halo, n_threads=n_threads)
+        traversals = make_traversals(
+            grid=field.grid, halo=halo, n_threads=n_threads_, left_first=left_first
+        )
         field.assemble(traversals)
         meta_and_data, fill_halos = field.impl
         sut = traversals._code["fill_halos_scalar"]  # pylint:disable=protected-access
 
         # act
         for thread_id in range(
-            n_threads
+            n_threads_
         ):  # TODO #96: xfail if not all threads executed?
             sut(thread_id, *meta_and_data, *fill_halos)
 
@@ -128,9 +136,9 @@ class TestPeriodicBoundaryCondition:
     @pytest.mark.parametrize("comp", DIMENSIONS)
     @pytest.mark.parametrize("dim_offset", (0, 1, 2))
     # pylint: disable=redefined-outer-name,too-many-arguments
-    def test_vector(data, halo, side, n_threads, comp, dim_offset):
+    def test_vector(data, halo, side, n_threads_, comp, dim_offset, left_first=True):
         n_dims = len(data)
-        if n_dims == 1 and n_threads > 1:
+        if n_dims == 1 and n_threads_ > 1:
             return
         if n_dims == 1 and (comp != INNER or dim_offset != 0):
             return
@@ -139,14 +147,16 @@ class TestPeriodicBoundaryCondition:
 
         # arrange
         field = VectorField(data, halo, tuple(Periodic() for _ in range(n_dims)))
-        traversals = make_traversals(grid=field.grid, halo=halo, n_threads=n_threads)
+        traversals = make_traversals(
+            grid=field.grid, halo=halo, n_threads=n_threads_, left_first=left_first
+        )
         field.assemble(traversals)
         meta_and_data, fill_halos = field.impl
         sut = traversals._code["fill_halos_vector"]  # pylint:disable=protected-access
 
         # act
         for thread_id in range(
-            n_threads
+            n_threads_
         ):  # TODO #96: xfail if not all threads executed?
             sut(thread_id, *meta_and_data, *fill_halos)
 

tests/unit_tests/test_boundary_condition_polar_2d.py
@@ -15,7 +15,7 @@ class TestPolarBoundaryCondition:
     @staticmethod
     @pytest.mark.parametrize("halo", (1,))
     @pytest.mark.parametrize("n_threads", (1, 2, 3))
-    def test_scalar_2d(halo, n_threads):
+    def test_scalar_2d(halo, n_threads_, left_first=True):
         # arrange
         data = np.array([[1, 6], [2, 7], [3, 8], [4, 9]], dtype=float)
         boundary_condition = (
@@ -23,8 +23,13 @@ class TestPolarBoundaryCondition:
             Polar(grid=data.shape, longitude_idx=OUTER, latitude_idx=INNER),
         )
         field = ScalarField(data, halo, boundary_condition)
+        # pylint:disable=duplicate-code
         traversals = Traversals(
-            grid=data.shape, halo=halo, jit_flags=JIT_FLAGS, n_threads=n_threads
+            grid=data.shape,
+            halo=halo,
+            jit_flags=JIT_FLAGS,
+            n_threads=n_threads_,
+            left_first=left_first,
         )
         field.assemble(traversals)
         meta_and_data, fill_halos = field.impl
@@ -32,7 +37,7 @@ class TestPolarBoundaryCondition:
 
         # act
         # pylint: disable-next=not-an-iterable
-        for thread_id in numba.prange(n_threads):
+        for thread_id in numba.prange(n_threads_):
             sut(thread_id, *meta_and_data, *fill_halos)
 
         # assert
@@ -48,7 +53,7 @@ class TestPolarBoundaryCondition:
     @staticmethod
     @pytest.mark.parametrize("halo", (1,))
     @pytest.mark.parametrize("n_threads", (1, 2, 3))
-    def test_vector_2d(halo, n_threads):
+    def test_vector_2d(halo, n_threads_, left_first=True):
         # arrange
         grid = (4, 2)
         data = (
@@ -78,7 +83,7 @@ class TestPolarBoundaryCondition:
         )
         field = VectorField(data, halo, boundary_conditions)
         traversals = Traversals(
-            grid=grid, halo=halo, jit_flags=JIT_FLAGS, n_threads=n_threads
+            grid=grid, halo=halo, jit_flags=JIT_FLAGS, n_threads=n_threads_, left_first=left_first,
         )
         field.assemble(traversals)
         meta_and_data, fill_halos = field.impl
@@ -86,7 +91,7 @@ class TestPolarBoundaryCondition:
 
         # act
         # pylint: disable-next=not-an-iterable
-        for thread_id in numba.prange(n_threads):
+        for thread_id in numba.prange(n_threads_):
             sut(thread_id, *meta_and_data, *fill_halos)
 
         # assert

tests/unit_tests/test_traversals.py
@@ -30,8 +30,10 @@ jit_flags = Options().jit_flags
 
 @lru_cache()
 # pylint: disable-next=redefined-outer-name
-def make_traversals(grid, halo, n_threads):
-    return Traversals(grid=grid, halo=halo, jit_flags=jit_flags, n_threads=n_threads)
+def make_traversals(grid, halo, n_threads, left_first):
+    return Traversals(
+        grid=grid, halo=halo, jit_flags=jit_flags, n_threads=n_threads, left_first=left_first
+    )
 
 
 @numba.njit(**jit_flags)
@@ -68,8 +70,8 @@ def _cell_id_vector(arg_1, arg_2, arg_3):
     return cell_id(*focus)
 
 
-def make_commons(grid, halo, num_threads):
-    traversals = make_traversals(grid, halo, num_threads)
+def make_commons(grid, halo, num_threads, left_first):
+    traversals = make_traversals(grid, halo, num_threads, left_first)
     n_dims = len(grid)
     halos = ((halo - 1, halo, halo), (halo, halo - 1, halo), (halo, halo, halo - 1))
 
@@ -93,10 +95,16 @@ class TestTraversals:
     @pytest.mark.parametrize("grid", ((3, 4, 5), (5, 6), (11,)))
     @pytest.mark.parametrize("loop", (True, False))
     # pylint: disable-next=redefined-outer-name
-    def test_apply_scalar(n_threads: int, halo: int, grid: tuple, loop: bool):
-        if len(grid) == 1 and n_threads > 1:
+    def test_apply_scalar(
+            n_threads_,
+            halo: int,
+            grid: tuple,
+            loop: bool,
+            left_first: bool = True,
+    ):
+        if len(grid) == 1 and n_threads_ > 1:
             return
-        cmn = make_commons(grid, halo, n_threads)
+        cmn = make_commons(grid, halo, n_threads_, left_first)
 
         # arrange
         sut = cmn.traversals.apply_scalar(loop=loop)
@@ -153,11 +161,11 @@ class TestTraversals:
     @staticmethod
     @pytest.mark.parametrize("halo", (1, 2, 3))
     @pytest.mark.parametrize("grid", ((3, 4, 5), (5, 6), (11,)))
-    # pylint: disable-next=redefined-outer-name
-    def test_apply_vector(n_threads: int, halo: int, grid: tuple):
-        if len(grid) == 1 and n_threads > 1:
+    # pylint: disable-next=too-many-locals
+    def test_apply_vector(n_threads_, halo: int, grid: tuple, left_first: bool = True):
+        if len(grid) == 1 and n_threads_ > 1:
             return
-        cmn = make_commons(grid, halo, n_threads)
+        cmn = make_commons(grid, halo, n_threads_, left_first)
 
         # arrange
         sut = cmn.traversals.apply_vector()

open-atmos/PyMPDATA

tests/unit_tests/test_boundary_condition_extrapolated_1d.py
@@ -15,12 +15,17 @@ class TestBoundaryConditionExtrapolated:
         "data",
         (np.array([1, 2, 3, 4], dtype=float), np.array([1, 2, 3, 4], dtype=complex)),
     )
-    def test_1d_scalar(data, n_threads=1, halo=1):
+    def test_1d_scalar(data, n_threads=1, halo=1, left_first=True):
         # arrange
         boundary_conditions = (Extrapolated(),)
         field = ScalarField(data, halo, boundary_conditions)
+        # pylint:disable=duplicate-code
         traversals = Traversals(
-            grid=field.grid, halo=halo, jit_flags=JIT_FLAGS, n_threads=n_threads
+            grid=field.grid,
+            halo=halo,
+            jit_flags=JIT_FLAGS,
+            n_threads=n_threads,
+            left_first=left_first,
         )
         field.assemble(traversals)
         meta_and_data, fill_halos = field.impl
@@ -36,12 +41,17 @@ class TestBoundaryConditionExtrapolated:
 
     @staticmethod
     @pytest.mark.parametrize("data", (np.array([1, 2, 3, 4], dtype=float),))
-    def test_1d_vector(data, n_threads=1, halo=2):
+    def test_1d_vector(data, n_threads=1, halo=2, left_first=True):
         # arrange
         boundary_condition = (Extrapolated(),)
         field = VectorField((data,), halo, boundary_condition)
+        # pylint:disable=duplicate-code
         traversals = Traversals(
-            grid=field.grid, halo=halo, jit_flags=JIT_FLAGS, n_threads=n_threads
+            grid=field.grid,
+            halo=halo,
+            jit_flags=JIT_FLAGS,
+            n_threads=n_threads,
+            left_first=left_first,
         )
         field.assemble(traversals)
         meta_and_data, fill_halos = field.impl

tests/unit_tests/test_boundary_condition_periodic_nd.py
@@ -50,8 +50,14 @@ JIT_FLAGS = Options().jit_flags
 
 @lru_cache()
 # pylint: disable-next=redefined-outer-name
-def make_traversals(grid, halo, n_threads):
-    return Traversals(grid=grid, halo=halo, jit_flags=JIT_FLAGS, n_threads=n_threads)
+def make_traversals(grid, halo, n_threads, left_first):
+    return Traversals(
+        grid=grid,
+        halo=halo,
+        jit_flags=JIT_FLAGS,
+        n_threads=n_threads,
+        left_first=left_first,
+    )
 
 
 class TestPeriodicBoundaryCondition:
@@ -67,8 +73,8 @@ class TestPeriodicBoundaryCondition:
     @pytest.mark.parametrize("halo", (1, 2, 3))
     @pytest.mark.parametrize("side", (LEFT, RIGHT))
     @pytest.mark.parametrize("dim", DIMENSIONS)
-    # pylint: disable-next=redefined-outer-name
-    def test_scalar(data, halo, side, n_threads, dim):
+    # pylint: disable-next=redefined-outer-name,too-many-arguments
+    def test_scalar(data, halo, side, n_threads, dim, left_first=True):
         n_dims = len(data.shape)
         if n_dims == 1 and dim != INNER:
             return
@@ -79,7 +85,9 @@ class TestPeriodicBoundaryCondition:
 
         # arrange
         field = ScalarField(data, halo, tuple(Periodic() for _ in range(n_dims)))
-        traversals = make_traversals(grid=field.grid, halo=halo, n_threads=n_threads)
+        traversals = make_traversals(
+            grid=field.grid, halo=halo, n_threads=n_threads, left_first=left_first
+        )
         field.assemble(traversals)
         meta_and_data, fill_halos = field.impl
         sut = traversals._code["fill_halos_scalar"]  # pylint:disable=protected-access
@@ -128,7 +136,7 @@ class TestPeriodicBoundaryCondition:
     @pytest.mark.parametrize("comp", DIMENSIONS)
     @pytest.mark.parametrize("dim_offset", (0, 1, 2))
     # pylint: disable=redefined-outer-name,too-many-arguments
-    def test_vector(data, halo, side, n_threads, comp, dim_offset):
+    def test_vector(data, halo, side, n_threads, comp, dim_offset, left_first=True):
         n_dims = len(data)
         if n_dims == 1 and n_threads > 1:
             return
@@ -139,7 +147,9 @@ class TestPeriodicBoundaryCondition:
 
         # arrange
         field = VectorField(data, halo, tuple(Periodic() for _ in range(n_dims)))
-        traversals = make_traversals(grid=field.grid, halo=halo, n_threads=n_threads)
+        traversals = make_traversals(
+            grid=field.grid, halo=halo, n_threads=n_threads, left_first=left_first
+        )
         field.assemble(traversals)
         meta_and_data, fill_halos = field.impl
         sut = traversals._code["fill_halos_vector"]  # pylint:disable=protected-access

tests/unit_tests/test_boundary_condition_polar_2d.py
@@ -15,7 +15,7 @@ class TestPolarBoundaryCondition:
     @staticmethod
     @pytest.mark.parametrize("halo", (1,))
     @pytest.mark.parametrize("n_threads", (1, 2, 3))
-    def test_scalar_2d(halo, n_threads):
+    def test_scalar_2d(halo, n_threads, left_first=True):
         # arrange
         data = np.array([[1, 6], [2, 7], [3, 8], [4, 9]], dtype=float)
         boundary_condition = (
@@ -23,8 +23,13 @@ class TestPolarBoundaryCondition:
             Polar(grid=data.shape, longitude_idx=OUTER, latitude_idx=INNER),
         )
         field = ScalarField(data, halo, boundary_condition)
+        # pylint:disable=duplicate-code
         traversals = Traversals(
-            grid=data.shape, halo=halo, jit_flags=JIT_FLAGS, n_threads=n_threads
+            grid=data.shape,
+            halo=halo,
+            jit_flags=JIT_FLAGS,
+            n_threads=n_threads,
+            left_first=left_first,
         )
         field.assemble(traversals)
         meta_and_data, fill_halos = field.impl
@@ -48,7 +53,7 @@ class TestPolarBoundaryCondition:
     @staticmethod
     @pytest.mark.parametrize("halo", (1,))
     @pytest.mark.parametrize("n_threads", (1, 2, 3))
-    def test_vector_2d(halo, n_threads):
+    def test_vector_2d(halo, n_threads, left_first=True):
         # arrange
         grid = (4, 2)
         data = (
@@ -77,8 +82,13 @@ class TestPolarBoundaryCondition:
             Polar(grid=grid, longitude_idx=OUTER, latitude_idx=INNER),
         )
         field = VectorField(data, halo, boundary_conditions)
+        # pylint:disable=duplicate-code
         traversals = Traversals(
-            grid=grid, halo=halo, jit_flags=JIT_FLAGS, n_threads=n_threads
+            grid=grid,
+            halo=halo,
+            jit_flags=JIT_FLAGS,
+            n_threads=n_threads,
+            left_first=left_first,
         )
         field.assemble(traversals)
         meta_and_data, fill_halos = field.impl

tests/unit_tests/test_traversals.py
@@ -30,8 +30,14 @@ jit_flags = Options().jit_flags
 
 @lru_cache()
 # pylint: disable-next=redefined-outer-name
-def make_traversals(grid, halo, n_threads):
-    return Traversals(grid=grid, halo=halo, jit_flags=jit_flags, n_threads=n_threads)
+def make_traversals(grid, halo, n_threads, left_first):
+    return Traversals(
+        grid=grid,
+        halo=halo,
+        jit_flags=jit_flags,
+        n_threads=n_threads,
+        left_first=left_first,
+    )
 
 
 @numba.njit(**jit_flags)
@@ -68,8 +74,8 @@ def _cell_id_vector(arg_1, arg_2, arg_3):
     return cell_id(*focus)
 
 
-def make_commons(grid, halo, num_threads):
-    traversals = make_traversals(grid, halo, num_threads)
+def make_commons(grid, halo, num_threads, left_first):
+    traversals = make_traversals(grid, halo, num_threads, left_first)
     n_dims = len(grid)
     halos = ((halo - 1, halo, halo), (halo, halo - 1, halo), (halo, halo, halo - 1))
 
@@ -92,11 +98,17 @@ class TestTraversals:
     @pytest.mark.parametrize("halo", (1, 2, 3))
     @pytest.mark.parametrize("grid", ((3, 4, 5), (5, 6), (11,)))
     @pytest.mark.parametrize("loop", (True, False))
-    # pylint: disable-next=redefined-outer-name
-    def test_apply_scalar(n_threads: int, halo: int, grid: tuple, loop: bool):
+    # pylint:disable=redefined-outer-name
+    def test_apply_scalar(
+        n_threads,
+        halo: int,
+        grid: tuple,
+        loop: bool,
+        left_first: bool = True,
+    ):
         if len(grid) == 1 and n_threads > 1:
             return
-        cmn = make_commons(grid, halo, n_threads)
+        cmn = make_commons(grid, halo, n_threads, left_first)
 
         # arrange
         sut = cmn.traversals.apply_scalar(loop=loop)
@@ -153,11 +165,11 @@ class TestTraversals:
     @staticmethod
     @pytest.mark.parametrize("halo", (1, 2, 3))
     @pytest.mark.parametrize("grid", ((3, 4, 5), (5, 6), (11,)))
-    # pylint: disable-next=redefined-outer-name
-    def test_apply_vector(n_threads: int, halo: int, grid: tuple):
+    # pylint: disable-next=too-many-locals,redefined-outer-name
+    def test_apply_vector(n_threads, halo: int, grid: tuple, left_first: bool = True):
         if len(grid) == 1 and n_threads > 1:
             return
-        cmn = make_commons(grid, halo, n_threads)
+        cmn = make_commons(grid, halo, n_threads, left_first)
 
         # arrange
         sut = cmn.traversals.apply_vector()