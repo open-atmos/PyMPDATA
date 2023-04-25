cfcc2ca9895059838e114d677623a7f0bc2ef92b
Kacper Derlatka <51274280+Delcior@users.noreply.github.com>
2023-04-25 20:55:00 +0200

    Upload plots as artefacts, upload anims to a "tip" release, remove leftover h5 file (#60)
    * remove obsolete file
    
    * upload plots
    
    * fix yaml
    
    * plot during computations
    
    * precommit
    
    * make gifs
    
    * fix syntax
    
    * precommit
    
    * fix pylint
    
    * fix path
    
    * fix test
    
    * fix
    
    * 11
    
    * precommit and pylint
    
    * generate first release
    
    * increase timeout even more
    
    * add missing sudo
    
    * remove apt-get install; add missing wildcards
    
    * fix frame path
    
    * reduce number of plots
    
    * fix mkdir
    
    * fix plot_path
    
    * fix path
    
    * fix paths
    
    * do rsvg-convert before convert
    
    * crop + border
    
    * uncomment all test cases
    
    * revert timeout change
    
    * less anim frames
    
    * fix output_steps enumaration
    
    * removing one test case (which likely does not add much insights)
    
    * fix anim urls
    
    ---------
    
    Co-authored-by: derlk <kacper.derlatka@pega.com>
    Co-authored-by: Sylwester Arabas <sylwester.arabas@uj.edu.pl>

--- tests/test_2d.py
+++ tests/test_2d.py
@@ -2,6 +2,10 @@
 # pylint: disable=missing-class-docstring,invalid-name,too-many-locals,too-many-arguments,c-extension-no-member
 # based on PyMPDATA README example
 
+import os
+import shutil
+from pathlib import Path
+
 import mpi4py
 import numba_mpi as mpi
 import numpy as np
@@ -48,47 +52,52 @@ class ReadmeSettings(Settings):
         ax.set_xlabel("x/dx")
         ax.set_ylabel("y/dy")
         ax.set_proj_type("ortho")
-        cnt = ax.contourf(xi + 0.5, yi + 0.5, psi, zdir="z", offset=-1, norm=norm)
+        cnt = ax.contourf(
+            xi + 0.5,
+            yi + 0.5,
+            psi,
+            zdir="z",
+            offset=-1,
+            norm=norm,
+            levels=np.linspace(*zlim, 11),
+        )
         cbar = pyplot.colorbar(cnt, pad=0.1, aspect=10, fraction=0.04)
         return cbar.norm
 
 
-@pytest.mark.parametrize(
-    "options_kwargs",
-    (
-        {"n_iters": 1},
-        {"n_iters": 2},
-        {"n_iters": 2, "third_order_terms": True},
-        {"n_iters": 2, "nonoscillatory": True},
-        {"n_iters": 3},
-    ),
+OPTIONS_KWARGS = (
+    {"n_iters": 1},
+    {"n_iters": 2, "third_order_terms": True},
+    {"n_iters": 2, "nonoscillatory": True},
+    {"n_iters": 3},
 )
+
+COURANT_FIELDS = (
+    (0.5, 0.25),
+    (-0.5, 0.25),
+    (0.5, -0.25),
+    (-0.5, -0.25),
+)
+
+
+@pytest.mark.parametrize("options_kwargs", OPTIONS_KWARGS)
 @pytest.mark.parametrize("n_threads", (1,))  # TODO #35 : 2+
-@pytest.mark.parametrize(
-    "output_steps",
-    (
-        (0,),
-        (0, 1),
-    ),
-)
-@pytest.mark.parametrize(
-    "courant_field",
-    (
-        (0.5, 0.25),
-        (-0.5, 0.25),
-        (0.5, -0.25),
-        (-0.5, -0.25),
-    ),
-)
+@pytest.mark.parametrize("courant_field", COURANT_FIELDS)
 def test_2d(
     mpi_tmp_path_fixed,
     options_kwargs,
     n_threads,
-    output_steps,
     courant_field,
+    output_steps=range(0, 25, 2),
     grid=(24, 24),
-    plot=False,
 ):  # pylint: disable=redefined-outer-name
+    plot = (
+        "CI_PLOTS_PATH" in os.environ
+        and courant_field == COURANT_FIELDS[-1]
+        and options_kwargs == OPTIONS_KWARGS[-1]
+    )
+
+    # arrange
     options_str = (
         str(options_kwargs)
         .replace(", ", "_")
@@ -108,9 +117,20 @@ def test_2d(
 
     settings = ReadmeSettings(output_steps)
 
+    # act
     for mpi_max_size, path in paths.items():
         truncated_size = min(mpi_max_size, mpi.size())
         rank = mpi.rank()
+
+        courant_str = str(courant_field).replace(" ", "")
+
+        plot_path = None
+        if plot:
+            plot_path = Path(os.environ["CI_PLOTS_PATH"]) / Path(
+                f"{options_str}_rank_{mpi.rank()}_size_{mpi.size()}_c_field_{courant_str}"
+            )
+            shutil.rmtree(plot_path, ignore_errors=True)
+            os.mkdir(plot_path)
 
         if rank == 0:
             Storage.create_dataset(
@@ -132,14 +152,26 @@ def test_2d(
                     courant_field=courant_field,
                 )
                 steps_done = 0
+                x_range = slice(*subdomain(grid[0], rank, truncated_size))
+
                 for i, output_step in enumerate(settings.output_steps):
                     n_steps = output_step - steps_done
                     simulation.advance(n_steps=n_steps)
                     steps_done += n_steps
-
-                    x_range = slice(*subdomain(grid[0], rank, truncated_size))
                     dataset[x_range, :, i] = simulation.advectee.get()
 
+                # plot
+                if plot:
+                    tmp = np.empty_like(dataset[:, :, -1])
+                    for i, _ in enumerate(settings.output_steps):
+                        tmp[:] = np.nan
+                        tmp[x_range, :] = dataset[x_range, :, i]
+                        settings.quick_look(tmp, zlim=(-1, 1))
+                        filename = f"step={i:04d}.svg"
+                        pyplot.savefig(plot_path / filename)
+                        pyplot.close()
+
+    # assert
     with barrier_enclosed():
         if mpi.rank() != 0:
             with Storage.non_mpi_contex(
@@ -147,13 +179,6 @@ def test_2d(
             ) as storage_expected, Storage.non_mpi_contex(
                 paths[mpi.rank() + 1], "r"
             ) as storage_actual:
-                settings.quick_look(
-                    storage_actual[dataset_name][:, :, -1], zlim=(-1, 1)
-                )
-                if plot:
-                    plot_path = f"{options_str}_threads_{n_threads}.pdf"
-                    pyplot.savefig(mpi_tmp_path_fixed / plot_path)
-                pyplot.close()
                 np.testing.assert_array_equal(
                     storage_expected[dataset_name][:, :, -1],
                     storage_actual[dataset_name][:, :, -1],
