03ac145766ef6edd307bf68d6f8a3e38b22ba64f
Sylwester Arabas <sylwester.arabas@agh.edu.pl>
2024-01-02 18:58:26 +0100

    mae pylint happiers

--- tests/smoke_tests/olesik_et_al_2022/test_wall_time.py
+++ tests/smoke_tests/olesik_et_al_2022/test_wall_time.py
@@ -5,6 +5,6 @@ import pytest
 from PyMPDATA_examples.Olesik_et_al_2022 import wall_time
 
 
-@pytest.mark.xfail(condition=(os.name == "nt"))
+@pytest.mark.xfail(condition=os.name == "nt")
 def test_wall_time():
     wall_time.test_wall_time()
