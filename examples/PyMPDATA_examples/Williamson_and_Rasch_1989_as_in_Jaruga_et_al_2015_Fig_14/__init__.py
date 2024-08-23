6f0e3e337636eedda61533e72df839b8b11374c4
pawel <pawel.magnu@gmail.com>
2024-08-23 21:59:44 +0200

    pylint fix 2

--- examples/PyMPDATA_examples/Williamson_and_Rasch_1989_as_in_Jaruga_et_al_2015_Fig_14/__init__.py
+++ examples/PyMPDATA_examples/Williamson_and_Rasch_1989_as_in_Jaruga_et_al_2015_Fig_14/__init__.py
@@ -3,6 +3,7 @@
 """
 This example is based on
 [Williamson and Rasch 1989](https://doi.org/10.1175/1520-0493(1989)117%3C0102:TDSLTW%3E2.0.CO;2).
-It demonstrates the use of `PyMPDATA.solver.g_factor` to transform the advection problem onto a sphere.
+It demonstrates the use of `PyMPDATA.solver.g_factor`
+to transform the advection problem onto a sphere.
 """
 pass
