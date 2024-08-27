8d27844fc62caeb5c66712a8f342dbaa41c573af
pawel <pawel.magnu@gmail.com>
2024-08-27 11:31:18 +0200

    disambiguate comment

--- examples/PyMPDATA_examples/Arabas_and_Farhat_2020/__init__.py
+++ examples/PyMPDATA_examples/Arabas_and_Farhat_2020/__init__.py
@@ -3,7 +3,7 @@ This module contains an implementation of a paper on pricing European and
 American options using the PyMPDATA library.
 The paper is available at: [Arabas and Farhat 2020](https://doi.org/10.1016/j.cam.2019.05.023).
 
-Each notebook in this module corresponds to a figure of a table in the paper.
+Each notebook in this directory corresponds to a figure or a table in the paper.
 I.e. the notebook `fig_1.ipynb` corresponds to Figure 1 in the paper.
 """
 
