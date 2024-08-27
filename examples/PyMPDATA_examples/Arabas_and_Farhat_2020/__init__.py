f84f0287e4eca9a5c30a4d24d7bc355267a4e33b
Sylwester Arabas <sylwester.arabas@agh.edu.pl>
2024-08-28 01:40:08 +0200

    shorten line

--- examples/PyMPDATA_examples/Arabas_and_Farhat_2020/__init__.py
+++ examples/PyMPDATA_examples/Arabas_and_Farhat_2020/__init__.py
@@ -1,5 +1,6 @@
 """
-This example implements simulations presented in the [Arabas and Farhat 2020](https://doi.org/10.1016/j.cam.2019.05.023)
+This example implements simulations presented in the 
+  [Arabas and Farhat 2020](https://doi.org/10.1016/j.cam.2019.05.023)
   study on pricing of European and American options using MPDATA.
 
 Each notebook in this directory corresponds to a figure or a table in the paper.
