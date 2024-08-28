97bb13947a9e62648c6616ad2bf5aecb2d9ced71
Sylwester Arabas <sylwester.arabas@agh.edu.pl>
2024-08-28 11:32:01 +0200

    add reference to the fig url from a docstring

--- examples/PyMPDATA_examples/Smolarkiewicz_2006_Figs_3_4_10_11_12/__init__.py
+++ examples/PyMPDATA_examples/Smolarkiewicz_2006_Figs_3_4_10_11_12/__init__.py
@@ -1,2 +1,5 @@
 """ example based on basic 1D advection test from
- [Smolarkiewicz 2006](https://doi.org/10.1002/fld.1071) """
+ [Smolarkiewicz 2006](https://doi.org/10.1002/fld.1071) 
+
+![plot](https://github.com/open-atmos/PyMPDATA/releases/download/tip/fig_4.svg)
+"""
