30c3f3cb0a1c8408323d68dfe33636773205dad4
Paweł Magnuszewski <47724273+pawelmagnu@users.noreply.github.com>
2024-08-29 14:13:19 +0200

    add doi links to pympdata_examples docstring (#442)
    Co-authored-by: Sylwester Arabas <sylwester.arabas@agh.edu.pl>

--- examples/PyMPDATA_examples/Jarecka_et_al_2015/__init__.py
+++ examples/PyMPDATA_examples/Jarecka_et_al_2015/__init__.py
@@ -1,3 +1,9 @@
+"""
+This module showcases the PyMPDATA implementation of an MPDATA-based shallow-water equations
+solver discussed and bencharked against analytical solutions in
+[Jarecka_et_al_2015](https://doi.org/10.1016/j.jcp.2015.02.003).
+"""
+
 from .plot_output import plot_output
 from .settings import Settings
 from .simulation import Simulation
--- examples/PyMPDATA_examples/Shipway_and_Hill_2012/__init__.py
+++ examples/PyMPDATA_examples/Shipway_and_Hill_2012/__init__.py
@@ -1,3 +1,9 @@
+"""
+This is an example of 2D droplet size-spectral/spatial problem of
+condensational growth in a column of air,
+as described in [Shipway and Hill 2012](https://doi.org/10.1002/qj.1913).
+"""
+
 from .arakawa_c import arakawa_c
 from .droplet_activation import DropletActivation
 from .formulae import convert_to
