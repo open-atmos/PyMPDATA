518b09d9844f152c683595583a7d9344f2797ccf
pawel <pawel.magnu@gmail.com>
2024-09-27 23:26:54 +0200

    merge markdown files for pympdata landing page

--- PyMPDATA/__init__.py
+++ PyMPDATA/__init__.py
@@ -1,25 +1,5 @@
 """
-Numba-accelerated Pythonic implementation of Multidimensional Positive Definite
-Advection Transport Algorithm (MPDATA) with examples in Python, Julia and Matlab
-
-PyMPDATA uses staggered grid with the following node placement for
-`PyMPDATA.scalar_field.ScalarField` and
-`PyMPDATA.vector_field.VectorField` elements:
-
-.. include:: ../docs/markdown/intro.md
-.. include:: ../docs/markdown/examples.md
-.. include:: ../docs/markdown/options.md
-.. include:: ../docs/markdown/grid.md
-.. include:: ../docs/markdown/python_grid.md
-.. include:: ../docs/markdown/grid_2.md
-.. include:: ../docs/markdown/julia_grid.md
-.. include:: ../docs/markdown/matlab_grid.md
-.. include:: ../docs/markdown/python_grid_2.md
-.. include:: ../docs/markdown/grid_3.md
-.. include:: ../docs/markdown/stepper_solver.md
-.. include:: ../docs/markdown/python_solver.md
-.. include:: ../docs/markdown/debugging_contribution.md
-
+.. include:: ../docs/markdown/pympdata_landing.md
 """
 
 # pylint: disable=invalid-name
