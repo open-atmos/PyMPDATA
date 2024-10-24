dc969ff5c09b513407c1e4eb4ff843c9815c8e5c
Paweł Magnuszewski <47724273+pawelmagnu@users.noreply.github.com>
2024-10-24 21:58:33 +0200

    Adding pdoc-generated example gallery used as both example docs landing site and setup.py description  (#467)
    Co-authored-by: Sylwester Arabas <sylwester.arabas@agh.edu.pl>

--- examples/PyMPDATA_examples/Smolarkiewicz_2006_Figs_3_4_10_11_12/__init__.py
+++ examples/PyMPDATA_examples/Smolarkiewicz_2006_Figs_3_4_10_11_12/__init__.py
@@ -3,5 +3,8 @@ This is an example of 1D homogeneous advection test from
 [Smolarkiewicz 2006](https://doi.org/10.1002/fld.1071), depicting infinite-gauge and
 flux-corrected transport case.
 
+demo.ipynb:
+.. include:: ./demo.ipynb.badges.md
+
 ![plot](https://github.com/open-atmos/PyMPDATA/releases/download/tip/fig_4.svg)
 """
