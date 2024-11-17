e9a0ff396a1e249317033d89a65c8d66379ddb03
Sylwester Arabas <sylwester.arabas@agh.edu.pl>
2024-11-17 13:32:43 +0100

    pylint, precommit, imports

--- examples/PyMPDATA_examples/utils/__init__.py
+++ examples/PyMPDATA_examples/utils/__init__.py
@@ -3,5 +3,6 @@ Various utility functions for PyMPDATA examples.
 """
 
 from open_atmos_jupyter_utils import show_plot
+
 from .merge_images_into_gif import merge_images_into_gif
 from .nondivergent_vector_field_2d import nondivergent_vector_field_2d
--- examples/PyMPDATA_examples/utils/merge_images_into_gif.py
+++ examples/PyMPDATA_examples/utils/merge_images_into_gif.py
@@ -1,11 +1,13 @@
 """ animation utilities """
 
 import os
+
 import imageio
 
+
 def merge_images_into_gif(image_folder, gif_name, duration=0.01):
-    """ creates a GIF file from a series of animation frames """
-    with imageio.get_writer(gif_name, mode='I', duration=duration, loop=0) as writer:
+    """creates a GIF file from a series of animation frames"""
+    with imageio.get_writer(gif_name, mode="I", duration=duration, loop=0) as writer:
         for filename in sorted(os.listdir(image_folder)):
             image = imageio.v3.imread(os.path.join(image_folder, filename))
             writer.append_data(image)
