0b047c6bf0c8c083d6026174d5c48adbc40ea2ec
Castlefox <michalmskowalczyk@mail.com>
2024-12-17 16:47:12 +0100

    vip_rhs check

--- examples/PyMPDATA_examples/Jaruga_et_al_2015/boussinesq.py
+++ examples/PyMPDATA_examples/Jaruga_et_al_2015/boussinesq.py
@@ -11,19 +11,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 from PyMPDATA_examples.Jaruga_et_al_2015.temp import *
 
-N, M  = 200, 200
-dx, dy = 10, 10
+N, M  = 20, 20
+dxy = 100, 100
 Tht_ref = 300
 g = 9.81
 r0 = 250
-nt = 200
-dt = 0.75
-
-options = Options(n_iters=2,infinite_gauge= True,nonoscillatory = True)
+nt = 10
+dt = 7.5
+beta  = 0.25
+options = Options(n_iters=2)#,infinite_gauge= True,nonoscillatory = True)
+assert options.n_halo == 1
 mesh = np.full((N,M), fill_value=Tht_ref, dtype=float)
 
 xi, yi = np.indices((N, M))
-mask = np.sqrt((xi*dx-1000)**2+(yi*dy-260)**2) < r0
+mask = np.sqrt((xi*dxy[0]-1000)**2+(yi*dxy[1]-260)**2) < r0
 mesh += np.where(mask, 0.5, 0)
 
 plt.imshow(mesh.T,origin='lower')
@@ -35,49 +36,65 @@ halo = options.n_halo
 bcond = (Periodic(), Periodic())
 
 field_ctor_kwargs = {'halo': halo, 'boundary_conditions': bcond}
+
+def new_sf(N,M):
+    return ScalarField(data=np.zeros((N,M)), **field_ctor_kwargs)
+
 advector = VectorField(data=(np.zeros((N+1, M)), np.zeros((N, M+1))), **field_ctor_kwargs)
 stepper = Stepper(options=options, grid=(N, M))
 
 solver_ctor_kwargs = {'stepper': stepper, 'advector': advector}
 solvers = {
     'tht': Solver(advectee=ScalarField(data=mesh, **field_ctor_kwargs), **solver_ctor_kwargs),
-    'u': Solver(advectee=ScalarField(data=np.zeros((N,M)), **field_ctor_kwargs), **solver_ctor_kwargs),
-    'w': Solver(advectee=ScalarField(data=np.zeros((N,M)), **field_ctor_kwargs), **solver_ctor_kwargs),
+    'u': Solver(advectee=new_sf(N,M), **solver_ctor_kwargs),
+    'w': Solver(advectee=new_sf(N,M), **solver_ctor_kwargs),
 }
 state_0 = solvers["tht"].advectee.get().copy()
 
 #actual going forward
-outfreq = 100
+outfreq = 1
 prs_tol = 1e-7;
+err_tol = prs_tol/dt
 output = []
-
+k_iters = 4
 rhs_w = np.zeros((N,M))
-stash = {'u': np.zeros((N,M)), 'w': np.zeros((N,M))}
+stash = {k: new_sf(N,M) for k in ('u', 'w')}
 vip_rhs = {'u': np.zeros((N,M)), 'w': np.zeros((N,M))}
-Phi = np.zeros((N,M))
-tmp_uvw = {'u': np.zeros((N,M)), 'w': np.zeros((N,M))}
+Phi = new_sf(N,M)
+tmp_uvw = {'u': new_sf(N,M), 'w': new_sf(N,M)}
+lap_tmp = {'u': new_sf(N,M), 'w': new_sf(N,M)}
+lap_err = np.zeros((N,M))
+err = new_sf(N,M)
+p_err = [new_sf(N,M) for _ in range(k_iters)]
+lap_p_err = [np.empty((N,M)) for _ in range(k_iters)]
 
+def debug(where):
+    psi = vip_rhs['w']
+    print(f"{where=} {np.amin(psi)=} {np.amax(psi)=}")
 
-hook_ante_loop(Phi,vip_rhs,solvers,tmp_uvw)#here
+hook_ante_loop(Phi,beta,vip_rhs,solvers,tmp_uvw,lap_tmp,err,p_err,lap_p_err,k_iters,err_tol,lap_err,dxy,N,M)#here
 for step in range(nt + 1):
     if step != 0:
-        print(advector.data[0])
         calc_gc_extrapolate_in_time(solvers, stash) # reads & writes to stash
-        calc_gc_interpolate_in_space(advector, stash,dt,(dx,dy)) # reads from stash
+        calc_gc_interpolate_in_space(advector, stash,dt,dxy) # reads from stash
         fill_stash(solvers, stash) # writes to stash
         apply_rhs(solvers["w"].advectee.get(), rhs_w, dt/2)
+        debug("A")
         vip_rhs_apply(dt,vip_rhs, solvers)
+        debug("B")
         for solver in solvers.values():
             solver.advance(n_steps=1)
         update_rhs(tht=solvers["tht"].advectee.get(), rhs_w=rhs_w, g=g, tht_ref=Tht_ref)
         apply_rhs(solvers["w"].advectee.get(), rhs_w, dt/2)
-        vip_rhs_impl_fnlz(vip_rhs,dt,solvers)
+        debug("C")
+        vip_rhs_impl_fnlz(vip_rhs,dt,solvers,err,Phi,beta,lap_tmp,tmp_uvw,p_err,lap_p_err,dxy,k_iters,err_tol,lap_err)
+        debug("D")
     if step % outfreq == 0:
         output.append(solvers["tht"].advectee.get().copy())
     print(step)
 
-plt.clf()
-plt.imshow(mesh.T,origin='lower')
-plt.colorbar()
-plt.show()
+    plt.clf()
+    plt.imshow(solvers["tht"].advectee.get().T,origin='lower')
+    plt.colorbar()
+    plt.show()
 
--- examples/PyMPDATA_examples/Jaruga_et_al_2015/temp.py
+++ examples/PyMPDATA_examples/Jaruga_et_al_2015/temp.py
@@ -1,94 +1,188 @@
+from pyexpat.errors import XML_ERROR_BAD_CHAR_REF
 from PyMPDATA_examples.Jarecka_et_al_2015 import Simulation
 import numpy as np
-
+from PyMPDATA.scalar_field import ScalarField
 from PyMPDATA.vector_field import VectorField
+
 def vip_rhs_apply(dt,vip_rhs, solvers):
      for k in ('u', 'w'):
-         solvers[k].advectee.get()[:] += 0.5 * dt * vip_rhs[k][:]
+         solvers[k].advectee.get()[:] += 0.5 * dt * vip_rhs[k]
          vip_rhs[k][:] = 0
 
 def calc_gc_extrapolate_in_time(solvers,stash):
     for k in ('u', 'w'):
-        stash[k] = -.5 * stash[k] + 1.5 * solvers[k].advectee.get()
-#    
+        stash[k].get()[:] = -.5 * stash[k].get() + 1.5 * solvers[k].advectee.get()
+        stash[k].get()[:] = -.5 * stash[k].get() + 1.5 * solvers[k].advectee.get()
+        xchng_pres(stash[k])   
 
 def calc_gc_interpolate_in_space(advector: VectorField,stash: dict,dt:float,dxy: tuple):
-
     idx = (
         (slice(1, -1), slice(None, None)),  # [1:-1, :]
         (slice(None, None), slice(1, -1))   # [:, 1:-1]
     )
-
     for axis, psi in enumerate(('u', 'w')):
         advector.get_component(axis)[idx[axis]] = dt/dxy[axis]*(
-            Simulation.interpolate(stash[psi], axis)
-        )    
+            Simulation.interpolate(stash[psi].get(), axis)
+        )
+    xchng(advector.get_component(0), x=True, y=False, h=1)
+    xchng(advector.get_component(1), x=False, y=True, h=1)
+
+
 def fill_stash(solvers, stash):
     for k in stash.keys():
-        stash[k][:] = solvers[k].advectee.get()
+        stash[k].get()[:] = solvers[k].advectee.get()
 
 def apply_rhs(w, rhs_w, dt):
     w += rhs_w * dt/2
 
+def ini_pressure(Phi,solvers,N,M):
+    npoints = N*M
+    for k in ('u', 'w'):
+        Phi.get()[:] -= 0.5 * np.power(solvers[k].advectee.get()[:],2)
+    Phi_mean = np.sum(Phi.get()) / npoints
+    Phi.get()[:] -= Phi_mean
 
+def xchng(data, x, y, h):
+    if x:
+        data[0:h,:] = data[-2*h:-h,:]
+        data[-h:,:] = data[h:2*h,:]
+    if y:
+        data[:,0:h] = data[:,-2*h:-h]
+        data[:,-h:] = data[:,h:2*h]
 
+def xchng_pres(Phi, x=True, y=True):
+    xchng(h = Phi.halo, x=x, y=y, data=Phi.data)
 
-def ini_pressure():
-    """
-    #Phi = 0
-    #npoints = 1
-    #for d in range(0,ndim):
-    #    Phi -= real_t(0.5) * np.power(""vips()[d](this->ijk)"",2)
-    #    npoints *= ""this->mem->distmem.grid_size[d]"" #Do I have this variable somewhere???
-    #Phi_mean = prs_sum(Phi, ijk) / npoints #is prs_sum implemented? shold I go from procedural to object programming or start procedural and make object?
-    #Phi -= Phi_mean
-    #return Phi
-    pass
-    """
 def update_rhs(*, tht, rhs_w, tht_ref, g):
     rhs_w[:] += g * (tht - tht_ref) / tht_ref
 
-def pressure_solver_update(simple = False):
-    pass
-def pressure_solver_apply():
-    pass
+def div(lap_tmp,dxy):
+    h = lap_tmp['u'].halo
+    return (
+        np.gradient(lap_tmp['u'].data, dxy[0],axis=0) +
+        np.gradient(lap_tmp['w'].data, dxy[1],axis=1)
+    )[h:-h,h:-h]
 
-def normalize_vip(solvers):
-    pass
+def lap(Phi, dxy, err_init, lap_tmp,tmp_uvw = None):
+    xchng_pres(Phi)
+    calc_grad(lap_tmp, Phi, dxy);
+    if (err_init):
+        for k in ('u', 'w'):
+            lap_tmp[k].get()[:] -= tmp_uvw[k].get()
 
-def calc_grad(tmp_uvw, Phi, dxy):
-    pass
-def hook_ante_loop(Phi,vip_rhs,solvers,tmp_uvw):
-       
-     # save_edges does nothing set edges too(empty function + no change in chart)
-     #xchng_pres seems to do nothing(no change in chart)
+    for k in ('u', 'w'):
+        xchng_pres(lap_tmp[k])
+        
+    return div(lap_tmp, dxy)
+
+def pressure_solver_loop_init(err,p_err,lap_p_err,dxy,lap_tmp,tmp_uvw):
+    p_err[0].get()[:] = err.get()[:]
+    lap_p_err[0][:] = lap(p_err[0], dxy, False, lap_tmp,tmp_uvw)
+
+def pressure_solver_loop_body(Phi,beta,converged,err,p_err,lap_p_err,dxy,k_iters,err_tol,lap_err,lap_tmp):
+    tmp_den = [1.]*k_iters
+    alpha = [1.]*k_iters
+    for v in range(0,k_iters):
+
+          tmp_den[v] = np.sum(lap_p_err[v]**2)
+          if (tmp_den[v] != 0):
+              beta = - np.dot(
+                  err.get().ravel(),
+                  lap_p_err[v].ravel()
+              ) / tmp_den[v]
+          Phi.get()[:] += beta * p_err[v].get()
+          err.get()[:] += beta * lap_p_err[v]
+
+          error = max(
+             abs(np.amax(err.get())),
+             abs(np.amin(err.get()))
+          )
+
+          if (error <= err_tol): 
+              converged = True
+
+          lap_err[:] = lap(err, dxy, False, lap_tmp);
+          
+          for l in range(v):
+              if (tmp_den[l] != 0):
+                    alpha[l] = - np.dot(lap_err.ravel(), lap_p_err[l].ravel()) / tmp_den[l];
+          if (v < (k_iters - 1)):
+              p_err[v + 1].get()[:] = err.get()
+              lap_p_err[v + 1][:] = lap_err[:]
+              for l in range(v):
+                  p_err[v + 1].get()[:] += alpha[l] * p_err[l].get()
+                  lap_p_err[v + 1][:] += alpha[l] * lap_p_err[l]
+          else:
+              p_err[0].get()[:] = err.get()[:] + alpha[0] * p_err[0].get()
+              lap_p_err[0][:] = lap_err[:] + alpha[0] * lap_p_err[0]
+              for l in range(1,v+1):
+                    p_err[0].get()[:] += alpha[l] * p_err[l].get()
+                    lap_p_err[0][:] += alpha[l] * lap_p_err[l]
+    return converged
+
+
+
+def pressure_solver_update(solvers,Phi,beta,lap_tmp,tmp_uvw,err,p_err,lap_p_err,dxy,k_iters,err_tol,lap_err,simple = False):
+    for k in ('u', 'w'):
+         tmp_uvw[k].get()[:] = solvers[k].advectee.get()
+    
+    #initial error
+    err.get()[:] = lap(Phi, dxy, True,lap_tmp,tmp_uvw)
+
+    iters = 0
+    converged = False
+
+    pressure_solver_loop_init(err,p_err,lap_p_err,dxy,lap_tmp,tmp_uvw)
+    #pseudo-time loop
+    while not converged:        
+         converged = pressure_solver_loop_body(Phi,beta,converged,err,p_err,lap_p_err,dxy,k_iters,err_tol,lap_err,lap_tmp)
+         iters += 1
+
+         if (iters > 10000): # going beyond 10000 iters means something is really wrong,
+             # usually boundary conditions but not always !
+             raise Exception("stuck in pressure solver")
+
+    xchng_pres(Phi)
+
+    calc_grad(tmp_uvw, Phi, dxy)
+
+
+def pressure_solver_apply(solvers,tmp_uvw):
+    for k in ('u', 'w'):
+        solvers[k].advectee.get()[:] -= tmp_uvw[k].get()
+
+
+def calc_grad(arg : ScalarField, Phi, dxy):
+    h = Phi.halo
+    idx = (slice(h,-h),slice(h,-h))
+    arg['u'].get()[:] = np.gradient(Phi.data,dxy[0],axis = 0)[idx]
+    arg['w'].get()[:] = np.gradient(Phi.data,dxy[1],axis = 1)[idx]
+
+def hook_ante_loop(Phi,beta,vip_rhs,solvers,tmp_uvw,lap_tmp,err,p_err,lap_p_err,k_iters,err_tol,lap_err,dxy,N,M):
      # correct initial velocity
-     Phi[:] = 0
-     #xchng_pres(Phi)
+     Phi.get()[:] = 0
 
-     pressure_solver_update(True)
+     pressure_solver_update(solvers,Phi,beta,lap_tmp,tmp_uvw,err,p_err,lap_p_err,dxy,k_iters,err_tol,lap_err,simple = True)
 
-     #xchng_pres(Phi);
+     xchng_pres(Phi)
      calc_grad(tmp_uvw, Phi, dxy)
-     pressure_solver_apply();
+     pressure_solver_apply(solvers,tmp_uvw)
 
      # potential pressure
-     ini_pressure();
+     ini_pressure(Phi,solvers,N,M)
 
      # allow pressure_solver_apply at the first time step
-     #xchng_pres(Phi)
-     calc_grad(tmp_uvw, Phi, this->ijk, this->dijk);
+     xchng_pres(Phi)
+     calc_grad(tmp_uvw, Phi, dxy)
      for k in ('u', 'w'):
-         vip_rhs[k][:]  -= tmp_uvw[k]
-            
+         vip_rhs[k][:] -= tmp_uvw[k].get()
 
          
-def vip_rhs_impl_fnlz(vip_rhs,dt,solvers):
+def vip_rhs_impl_fnlz(vip_rhs,dt,solvers,err,Phi,beta,lap_tmp,tmp_uvw,p_err,lap_p_err,dxy,k_iters,err_tol,lap_err):
     for k in ('u', 'w'):
         vip_rhs[k][:] = -solvers[k].advectee.get()
-    pressure_solver_update()
-    pressure_solver_apply()
-    #normalize_vip(solvers) connected to xchng_pres only
+    pressure_solver_update(solvers,Phi,beta,lap_tmp,tmp_uvw,err,p_err,lap_p_err,dxy,k_iters,err_tol,lap_err)
+    pressure_solver_apply(solvers,tmp_uvw)
 
     for k in ('u', 'w'):
         vip_rhs[k][:] += solvers[k].advectee.get()
