b2b1904d208200eab77acf325fc168fbe5b96dee
pawel <pawel.magnu@gmail.com>
2025-01-15 21:18:33 +0100

    moving changes to another branch

--- examples/PyMPDATA_examples/Arabas_and_Farhat_2020/setup1_european_corridor.py
+++ examples/PyMPDATA_examples/Arabas_and_Farhat_2020/setup1_european_corridor.py
@@ -1,5 +1,5 @@
 import numpy as np
-import PyMPDATA_examples.utils.financial_formulae.Black_Scholes_1973 as BS73
+import PyMPDATA_examples.Arabas_and_Farhat_2020.Black_Scholes_1973 as BS73
 from pystrict import strict
 
 
