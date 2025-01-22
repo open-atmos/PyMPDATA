fad57814a63fd495342f547543fdf1d02d1a6e2d
pawel <pawel.magnu@gmail.com>
2025-01-22 17:06:50 +0100

    changed naming

--- tests/smoke_tests/utils/test_financial_formulae.py
+++ tests/smoke_tests/utils/test_financial_formulae.py
@@ -1,4 +1,5 @@
-# pylint: disable=missing-module-docstring,missing-class-docstring,missing-function-docstring
+# pylint: disable=missing-module-docstring,missing-class-docstring,missing-function-docstring,invalid-name
+# pylint: disable=too-many-arguments
 import numpy as np
 import pytest
 from PyMPDATA_examples.utils.financial_formulae import Black_Scholes_1973 as BS73
@@ -14,18 +15,32 @@ class TestFinancialFormulae:
             {"normal": BS73.p_euro, "with_dividend": BS73.p_euro_with_dividend},
         ),
     )
-    @pytest.mark.parametrize("S", (np.array([95, 100, 105]),))
-    @pytest.mark.parametrize("K", (100, 10))
-    @pytest.mark.parametrize("T", (1, 0.5))
-    @pytest.mark.parametrize("r", (0.05, 0.001))
-    @pytest.mark.parametrize("sgma", (0.2, 0.5))
+    @pytest.mark.parametrize("price", (np.array([95, 100, 105]),))
+    @pytest.mark.parametrize("strike", (100, 10))
+    @pytest.mark.parametrize("time_to_maturity", (1, 0.5))
+    @pytest.mark.parametrize("risk_free_rate", (0.05, 0.001))
+    @pytest.mark.parametrize("volatility", (0.2, 0.5))
     @pytest.mark.parametrize("dividend_yield", (0.02, 0))
-    def test_black_scholes_with_dividend(funs: dict, S, K, T, r, sgma, dividend_yield):
-        common_args = {"S": S, "K": K, "T": T, "sgma": sgma, "r": r}
+    def test_black_scholes_with_dividend(
+        funs: dict,
+        price,
+        strike,
+        time_to_maturity,
+        risk_free_rate,
+        volatility,
+        dividend_yield,
+    ):
+        common_args = {
+            "S": price,
+            "K": strike,
+            "T": time_to_maturity,
+            "sgma": volatility,
+            "r": risk_free_rate,
+        }
         price_dividend = funs["with_dividend"](
             dividend_yield=dividend_yield, **common_args
         )
-        price_normal = funs["normal"](b=r - dividend_yield, **common_args)
+        price_normal = funs["normal"](b=risk_free_rate - dividend_yield, **common_args)
         assert np.allclose(price_dividend, price_normal)
 
     @staticmethod
