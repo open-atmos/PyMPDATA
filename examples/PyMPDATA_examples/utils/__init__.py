f914599286cd36dc6ebd80ab59081532c3661e28
Sylwester Arabas <sylwester.arabas@agh.edu.pl>
2025-01-28 21:03:42 +0100

    leftover cleanups

--- examples/PyMPDATA_examples/utils/__init__.py
+++ examples/PyMPDATA_examples/utils/__init__.py
@@ -5,4 +5,3 @@ Various utility functions for PyMPDATA examples.
 from open_atmos_jupyter_utils import show_plot
 
 from .nondivergent_vector_field_2d import nondivergent_vector_field_2d
-from .notebook_vars import notebook_vars
