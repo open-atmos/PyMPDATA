1c2627b97a328a47aa273233e936dad5e1286931
Sylwester Arabas <sylwester.arabas@agh.edu.pl>
2025-01-28 20:45:47 +0100

    restrict ipykernel version

--- setup.py
+++ setup.py
@@ -79,6 +79,7 @@ setup(
             ),
             "jupyter-core" + ("<5.0.0" if CI else ""),
             "ipywidgets" + ("!=8.0.3" if CI else ""),
+            "ipykernel" + ("<6.22.0" if CI else ""),
             "ghapi",
             "pytest",
             "pytest-benchmark",
