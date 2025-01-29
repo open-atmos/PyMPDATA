7f304916666252efb9b0533def4848ef1e37977b
Sylwester Arabas <sylwester.arabas@agh.edu.pl>
2025-01-29 10:08:21 +0100

    precommit

--- examples/PyMPDATA_examples/Jaruga_et_al_2015/__init__.py
+++ examples/PyMPDATA_examples/Jaruga_et_al_2015/__init__.py
@@ -1,7 +1,7 @@
 """
 This module showcases a PyMPDATA implementation of an MPDATA-based Boussinesq system solver
 with Poisson equation for the pressure-term solved using a bespoke (libmpdata++-based) implementation
-of the generalised conjugate-residual scheme. Simulation setup based on 
+of the generalised conjugate-residual scheme. Simulation setup based on
 [Smolarkiewicz & Pudykiewicz 1992](https://doi.org/10.1175/1520-0469(1992)049%3C2082:ACOSLA%3E2.0.CO;2)
 (following Fig 19 in [Jaruga_et_al_2015](https://doi.org/10.5194/gmd-8-1005-2015)).
 
