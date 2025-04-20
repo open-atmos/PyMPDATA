65f7774914c63460750cf1950ef845c79b734b3e
sfonxu <wronski.michal4@wp.pl>
2025-04-20 15:17:08 +0200

    remove duplicate code warning from test_upwind_2d

--- tests/unit_tests/test_upwind_2d.py
+++ tests/unit_tests/test_upwind_2d.py
@@ -1,4 +1,4 @@
-# pylint: disable=missing-module-docstring,missing-class-docstring,missing-function-docstring
+# pylint: disable=missing-module-docstring,missing-class-docstring,missing-function-docstring,duplicate-code
 import numpy as np
 import pytest
 
