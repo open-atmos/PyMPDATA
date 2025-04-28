20ca5933fb024449bddbd3f2c930f2b040a87a83
pawel <pawel.magnu@gmail.com>
2025-04-28 13:12:26 +0200

    adressed some issues before merge

--- examples/PyMPDATA_examples/Magnuszewski_et_al_2025/asian_option.py
+++ examples/PyMPDATA_examples/Magnuszewski_et_al_2025/asian_option.py
@@ -17,43 +17,24 @@ from PyMPDATA.impl.enumerations import (
 )
 from PyMPDATA.impl.traversals_common import make_fill_halos_loop
 
-OPTIONS = {
-    "n_iters": 3,
-    "infinite_gauge": False,
-    "nonoscillatory": True,
-    "divergent_flow": False,
-    "third_order_terms": False,
-    "non_zero_mu_coeff": True,
-}
+
 # pylint: disable=too-few-public-methods, too-many-lines, global-statement, abstract-method
-
-
 class Settings:
-    params = SimpleNamespace(
-        T=1,
-        sgma=0.1,
-        r=0.1,
-        K=100,
-    )
-
-    S_min = 50
-    S_max = 200
-
     def __init__(self, T, sgma, r, K, S_min, S_max):
-        self.params.T = T
-        self.params.sgma = sgma
-        self.params.r = r
-        self.params.K = K
+        self.T = T
+        self.sgma = sgma
+        self.r = r
+        self.K = K
         self.S_min = S_min
         self.S_max = S_max
         self.rh = None
 
     def payoff(self, A: np.ndarray, da: np.float32 = 0, variant="call"):
         def call(x):
-            return np.maximum(0, x - self.params.K)
+            return np.maximum(0, x - self.K)
 
         def put(x):
-            return np.maximum(0, self.params.K - x)
+            return np.maximum(0, self.K - x)
 
         if variant == "call":
             payoff_func = call
@@ -112,12 +93,12 @@ def _make_scalar_custom(
 
 
 class Simulation:
-    def __init__(self, settings, *, nx, ny, nt, variant="call"):
+    def __init__(self, settings, *, nx, ny, nt, OPTIONS, variant="call"):
         self.nx = nx
         self.nt = nt
         self.settings = settings
         self.ny = ny
-        self.dt = settings.params.T / self.nt
+        self.dt = settings.T / self.nt
         log_s_min = np.log(settings.S_min)
         log_s_max = np.log(settings.S_max)
         self.S = np.exp(np.linspace(log_s_min, log_s_max, self.nx))
@@ -127,10 +108,8 @@ class Simulation:
         self.dx = (log_s_max - log_s_min) / self.nx
         self.settings = settings
         self.step_number = 0
-        sigma_squared = pow(settings.params.sgma, 2)
-        courant_number_x = (
-            -(0.5 * sigma_squared - settings.params.r) * (-self.dt) / self.dx
-        )
+        sigma_squared = pow(settings.sgma, 2)
+        courant_number_x = -(0.5 * sigma_squared - settings.r) * (-self.dt) / self.dx
         self.l2 = self.dx * self.dx / sigma_squared / self.dt
         self.mu_coeff = (0.5 / self.l2, 0)
         assert (
@@ -152,7 +131,7 @@ class Simulation:
             stepper=stepper,
             advectee=ScalarField(
                 self.payoff_2d.astype(dtype=options.dtype)
-                * np.exp(-self.settings.params.r * self.settings.params.T),
+                * np.exp(-self.settings.r * self.settings.T),
                 halo=options.n_halo,
                 boundary_conditions=self.boundary_conditions,
             ),
@@ -174,26 +153,25 @@ class Simulation:
 
     @property
     def boundary_conditions(self):
-        raise NotImplementedError()
+        return (
+            Extrapolated(OUTER),
+            Extrapolated(INNER),
+        )
 
     def add_half_rhs(self):
-        raise NotImplementedError()
+        pass
 
     def free_boundary(self, t):
-        raise NotImplementedError()
+        pass
 
     def step(self):
         global _t
-        _t = self.settings.params.T - (self.step_number + 0.5) * self.dt
-
+        _t = self.settings.T - (self.step_number + 0.5) * self.dt
         self.add_half_rhs()
         self.free_boundary(t=_t)
-
         self.solver.advance(1, self.mu_coeff)
-
         self.add_half_rhs()
         self.free_boundary(t=_t)
-
         self.step_number += 1
 
 
@@ -202,72 +180,11 @@ class _Asian(Simulation):
     def payoff_2d(self):
         return np.repeat([self.payoff], self.nx, axis=0)
 
-    def free_boundary(self, t):
-        pass
-
-    @cached_property
-    def boundary_conditions(self):
-        return (
-            Extrapolated(OUTER),
-            Extrapolated(INNER),
-        )
-
 
 class AsianArithmetic(_Asian):
     @property
     def a_dim_advector(self):
         a_dim_advector = np.zeros((self.nx, self.ny + 1))
         for i in range(self.ny + 1):
-            a_dim_advector[:, i] = -self.dt / self.dy * self.S / self.settings.params.T
+            a_dim_advector[:, i] = -self.dt / self.dy * self.S / self.settings.T
         return a_dim_advector
-
-    def add_half_rhs(self):
-        pass
-
-
-class AsianGeometric(_Asian):
-    @property
-    def a_dim_advector(self):
-        a_dim_advector = np.zeros((self.nx, self.ny + 1))
-        A_edg = np.arange(self.ny + 1) * self.dy
-        for i in range(self.ny + 1):
-            a_dim_advector[:, i] = (
-                -self.dt
-                / self.dy
-                * np.log(self.S)
-                * (A_edg[i] - self.dy / 2)
-                / self.settings.params.T
-            )
-        return a_dim_advector
-
-    def add_half_rhs(self):
-        pass
-
-
-class European(Simulation):
-    @cached_property
-    def a_dim_advector(self):
-        return np.zeros((self.nx, self.ny + 1))
-
-    def add_half_rhs(self):
-        pass
-
-    @cached_property
-    def payoff_2d(self):
-        return np.repeat(self.payoff.reshape(self.nx, 1), self.ny, axis=1)
-
-    def free_boundary(self, t):
-        pass
-
-    @cached_property
-    def boundary_conditions(self):
-        return (
-            Extrapolated(OUTER),
-            Extrapolated(INNER),
-        )
-
-
-class American(European):
-    def free_boundary(self, t):
-        psi = self.solver.advectee.get()
-        psi[:] = np.maximum(psi, self.payoff_2d / np.exp(self.settings.params.r * t))
