999c89b26acd28f55eb81588478cab3ee906cd26
sfonxu <wronski.michal4@wp.pl>
2025-05-08 13:27:22 +0200

    change clock.py implementation and python CI version to 3.12

--- PyMPDATA/impl/clock.py
+++ PyMPDATA/impl/clock.py
@@ -2,7 +2,25 @@
 no time unit guaranteed, returned value only for relative time measurements"""
 
 import ctypes
+import sys
 
-clock = ctypes.pythonapi._PyTime_GetSystemClock  # pylint:disable=protected-access
-clock.argtypes = []
-clock.restype = ctypes.c_int64
+import numba
+import numpy as np
+
+if sys.version_info < (3, 13):
+    clock = ctypes.pythonapi._PyTime_GetSystemClock  # pylint:disable=protected-access
+    clock.argtypes = []
+    clock.restype = ctypes.c_int
+else:
+    clock_impl = ctypes.pythonapi.PyTime_Time
+    clock_impl.argtypes = [ctypes.c_void_p]
+    clock_impl.restype = ctypes.c_int
+
+    assert ctypes.c_time_t == ctypes.c_int64
+
+    @numba.jit(nopython=True)
+    def clock():
+        """Numba-JITable version of clock function for Python > 3.12"""
+        result = np.empty(shape=(1,), dtype=np.int64)
+        clock_impl(result.ctypes)
+        return result
