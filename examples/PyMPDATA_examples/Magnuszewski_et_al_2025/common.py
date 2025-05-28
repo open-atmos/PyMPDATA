5adca3bf2f13a3900f26805df6cdec0ebc64e163
Sylwester Arabas <sylwester.arabas@agh.edu.pl>
2025-05-29 01:17:26 +0200

    add 4-iters version to figs; use 2-iter in table; refactors

--- examples/PyMPDATA_examples/Magnuszewski_et_al_2025/common.py
+++ examples/PyMPDATA_examples/Magnuszewski_et_al_2025/common.py
@@ -5,8 +5,13 @@ OPTIONS = {
         n_iters=1,
         non_zero_mu_coeff=True,
     ),
-    "MPDATA": Options(
-        n_iters=3,
+    "MPDATA (2 it.)": Options(
+        n_iters=2,
+        nonoscillatory=True,
+        non_zero_mu_coeff=True,
+    ),
+    "MPDATA (4 it.)": Options(
+        n_iters=4,
         nonoscillatory=True,
         non_zero_mu_coeff=True,
     ),
