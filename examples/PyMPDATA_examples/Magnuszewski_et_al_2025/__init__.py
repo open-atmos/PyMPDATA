7caaa9eff1ed6d9f4903fe8eeb391f1904623855
Sylwester Arabas <sylwester.arabas@agh.edu.pl>
2025-06-02 21:07:30 +0200

    add DOI citations for Magnuszewski 2025 example (arXiv preprint is out)

---
+++ examples/PyMPDATA_examples/Magnuszewski_et_al_2025/__init__.py
@@ -0,0 +1,10 @@
+"""
+Path-dependent option pricing with two-dimensional PDE using MPDATA
+([Magnuszewski and Arabas 2025](https://doi.org/10.48550/arXiv.2505.24435))
+
+figs.ipynb:
+.. include:: ./figs.ipynb.badges.md
+
+table_1.ipynb:
+.. include:: ./table_1.ipynb.badges.md
+"""
