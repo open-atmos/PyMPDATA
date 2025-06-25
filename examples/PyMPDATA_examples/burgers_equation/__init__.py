8a484286833def2e1ed3b0105ee35a5d2299f698
Sylwester Arabas <sylwester.arabas@agh.edu.pl>
2025-06-25 22:44:32 +0200

    add header cells to the notebook, some cleanups

--- examples/PyMPDATA_examples/burgers_equation/__init__.py
+++ examples/PyMPDATA_examples/burgers_equation/__init__.py
@@ -4,3 +4,11 @@ PyMPDATA 1D Burgers' equation example with gif creation.
 burgers-equation.ipynb:
 .. include:: ./burgers_equation.ipynb.badges.md
 """
+from .burgers_equation import (
+    calculate_analytical_solutions,
+    plot_analytical_solutions,
+    plot_numerical_vs_analytical,
+    plot_gif,
+    run_numerical_simulation,
+    NT, NX, T_MAX, T_RANGE, T_SHOCK,
+)
