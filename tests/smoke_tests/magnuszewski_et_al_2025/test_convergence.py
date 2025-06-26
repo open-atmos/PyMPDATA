deda49ae6c1bdb7108ae862294676de5ef4e77ba
pawelmagnu <pawel.magnu@gmail.com>
2025-06-26 16:57:56 +0200

    added tests for convergence analysis

---
+++ tests/smoke_tests/magnuszewski_et_al_2025/test_convergence.py
@@ -0,0 +1,49 @@
+"""
+tests for Asian (path-dependent) option pricing example using 2D advection-diffusion PDE
+"""
+
+from pathlib import Path
+
+import pytest
+from open_atmos_jupyter_utils import notebook_vars
+from PyMPDATA_examples import Magnuszewski_et_al_2025
+
+PLOT = False
+
+
+@pytest.fixture(scope="session", name="variables")
+def _variables_fixture():
+    return notebook_vars(
+        file=Path(Magnuszewski_et_al_2025.__file__).parent
+        / "convergence_analysis.ipynb",
+        plot=PLOT,
+    )
+
+
+def _datasets(variables):
+    return {
+        "upwind": variables["l2_errors"]["UPWIND"],
+        "mpdata": variables["l2_errors"]["MPDATA"],
+        "error_upwind": variables["simulated_errors_upwind"],
+        "error_mpdata_2nd": variables["simulated_errors_mpdata_t2"],
+        "error_mpdata_3rd": variables["simulated_errors_mpdata_t3"],
+    }
+
+
+class TestFigs:
+    """basic assertions for convergence analysis notebook"""
+
+    @staticmethod
+    @pytest.mark.parametrize(
+        "lower, higher",
+        (
+            ("error_upwind", "upwind"),
+            ("mpdata", "upwind"),
+            ("mpdata", "error_mpdata_2nd"),
+            ("error_mpdata_3rd", "mpdata"),
+        ),
+    )
+    def test_order_of_lines(variables, lower, higher):
+        """checks if a given set of points is above/below another one"""
+        data = _datasets(variables)
+        assert data[lower] <= data[higher]
