a88adbfc78a4f280e24987c880e60638d8d58c8a
Sylwester Arabas <sylwester.arabas@agh.edu.pl>
2025-11-14 00:19:59 +0100

    New assertion to ensure version compatibility between PySDM and PySDM_examples #1733

--- examples/PyMPDATA_examples/__init__.py
+++ examples/PyMPDATA_examples/__init__.py
@@ -3,9 +3,12 @@
 """
 
 from importlib.metadata import PackageNotFoundError, version
+import PyMPDATA
 
 try:
     __version__ = version(__name__)
 except PackageNotFoundError:
     # package is not installed
     pass
+
+assert PyMPDATA.__version__ == __version__
