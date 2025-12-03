8adea7cc286b2376b96eb416f337509ce7520248
Sfonxu <wronski.michal4@wp.pl>
2025-12-03 17:07:38 +0100

    Update regex to catch "0.0" and such that appear on CI

--- examples/PyMPDATA_examples/__init__.py
+++ examples/PyMPDATA_examples/__init__.py
@@ -13,7 +13,7 @@ except PackageNotFoundError:
     # package is not installed
     pass
 
-validator = r"[0-9]+\.[0-9]+\.[0-9]+"
+validator = r"[0-9].+[0-9]"
 PyMPDATA_ver = re.search(validator, PyMPDATA.__version__)[0]
 examples_ver = re.search(validator, __version__)[0]
 assert PyMPDATA_ver == examples_ver
